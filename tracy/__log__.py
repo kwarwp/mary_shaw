e1c63f84db4fcd6491309c58391d1f77e1905c64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:04:48 -0300

    Saved tracy/__log__.py @2019-09-25 15:04

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -32,3 +32,19 @@ Traceback (most recent call last):
     outro_jogo.incia()
 AttributeError: 'JogoTesouroInca' object has no attribute 'incia'
 '''},
+{'date': 'Wed Sep 25 2019 12:04:34.78 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    outro_jogo = JogoMarilia()
+NameError: name 'JogoMarilia' is not defined
+'''},
