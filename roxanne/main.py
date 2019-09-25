e373f1d93ce93337c4a6aee8c337852472c2ec4b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:43:47 -0300

    Saved roxanne/main.py @2019-09-25 14:43

--- roxanne/main.py
+++ roxanne/main.py
@@ -11,3 +11,8 @@ class JogoTesouroInca:
         """ Inicia a construção do Jogo """
         self.cena_do_templo = "Templo do tesouro Inca"
         print("Descrição:", self.cena_do_templo)
+        
+        
+if __name__ == "__main__":
+    jogo = JogoTesouroInca()
+    outro_jogo = JogoTesouroInca()
