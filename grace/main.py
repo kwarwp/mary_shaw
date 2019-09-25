57525282b5899eb1ae702b6d499683ca59d25aaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:44:15 -0300

    Saved grace/main.py @2019-09-25 14:44

--- grace/main.py
+++ grace/main.py
@@ -1,1 +1,16 @@
 # mary_shaw.grace.main.py
+"""
+    Jogo do Tesouro Inca
+"""
+__author__ = "Andre"
+
+class jogoTesouroInca:
+    """ Representa o jogo principal """
+    def inicia(self):
+        """ Inicia a construção do jogo """
+        self.cena_do_templo = "Templo do Tesouro Inca"
+        print("Descrição:", self.cena_do_templo)
+        
+if __name__ == "__main__":
+    jogo = JogoTesouroInca()
+    outro_jogo = JogoTesouroInca()
