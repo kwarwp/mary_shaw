6ee99aec97f880da1660199b8db56b464cecfe13
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:04:37 -0300

    Saved tracy/main.py @2019-09-25 15:04

--- tracy/main.py
+++ tracy/main.py
@@ -1,20 +1,25 @@
-# mary_shaw.tracy.main.py
-from adda.main import JogoTesouroInca as JogoMarilia
+# mary_shaw.Tracy.main.py
+#from adda.main import JogoTesouroInca as JogoMarilia
+
 """
     Tesouro Inca
 """
-__author__ = "Lorena P Grion"
+from _spy.vitollino.main import Cena
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/zsytFm2.jpg"
+
+__author__ = "Carlo"
 
 
 class JogoTesouroInca:
-    """ Representa a classe do Jogo principal """
+    """ Representa o Jogo principal """
     def inicia(self):
         """ Inicia a construção do Jogo """
-        self.cena_do_templo = "Templo do tesouro Inca"
-        print("Descrição:", self.cena_do_templo)
+        self.cena_do_templo = "Templo perdido do tesouro Inca"
+        print("Descrição:", self.cena_do_templo, __name__)
+        
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
     outro_jogo = JogoMarilia()
     jogo.inicia()
-    outro_jogo.incia()
+    outro_jogo.inicia()
