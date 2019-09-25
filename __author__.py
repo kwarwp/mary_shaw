a8efbb4e94e5819a0a5e4718af340b63e444a308
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 17:46:23 -0300

    Saved __author__.py @2019-09-25 17:46

--- __author__.py
+++ __author__.py
@@ -1,2 +1,3 @@
 {
 "mary_shaw": {"author_nick": "POO PPGI 19.3", "author_name": "Labase - Disciplina de ProgramaÃ§Ã£o Orientada a Objetos - PPGI 2019-3", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
+"roxanne": {"author_nick": "T_Inca_Carlo", "author_name": "Labase - Disciplina de ProgramaÃ§Ã£o Orientada a Objetos - PPGI 2019-3", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
