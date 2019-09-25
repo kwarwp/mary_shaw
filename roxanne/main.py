38ddc3d76aff338aa485d0682ebdc4f44987d711
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:39:31 -0300

    Created roxanne/main.py @2019-09-25 14:39

---
+++ roxanne/main.py
@@ -0,0 +1,13 @@
+# mary_shaw.roxanne.main.py
+"""
+    Tesouro Inca
+"""
+__author__ = "Carlo"
+
+
+class JogoTesouroInca:
+    """ Representa o Jogo principal """
+    def inicia(self):
+        """ Inicia a construção do Jogo """
+        self.cena_do_templo = "Templo do tesouro Inca"
+        print("Descrição:", self.cena_do_templo)
