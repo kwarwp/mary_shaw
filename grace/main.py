25d153d92b81df54841d26cf50043b08d8efaa7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:11:41 -0300

    Saved grace/main.py @2019-09-25 15:11

--- grace/main.py
+++ grace/main.py
@@ -1,18 +1,22 @@
 # mary_shaw.grace.main.py
-from spy.vitollino.main import Cena
+from _spy.vitollino.main import Cena
 """
     Jogo do Tesouro Inca
 """
 __author__ = "Andre"
 
-IMAGEM_DO_TEMPLO = "https://i.imgur.com/pq242D7.jpg"
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/pq242D7.jpg" #É UMA CONSTANTE
 
 class JogoTesouroInca:
     """ Representa o jogo principal """
+    def __init__(self):
+        """Constroi a cena"""
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+
     def inicia(self):
         """ Inicia a construção do jogo """
-        self.cena_do_templo = "Templo do Tesouro Inca"
-        print("Descrição:", self.cena_do_templo)
+        self.cena_do_templo.vai()
+        
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
