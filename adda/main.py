1d04f5666b003520aa7e5622a766c6658cb2af7b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:39:40 -0300

    Created adda/main.py @2019-09-25 14:39

---
+++ adda/main.py
@@ -0,0 +1,15 @@
+# mary_shaw.adda.main.py
+# http://supygirls.pythonanywhere.com/
+"""
+   Tesouro Inca
+   
+"""
+__author__= "Marilia Campos Galvao"
+
+#camel case só se usa em nome de classe
+class JogoTesouroInca:
+    """ Representa o Jogo principal """
+    def inicia(self):
+        """ Inicia a construção do jogo """
+        self.cena_do_templo = "Templo do tesouro Inca"
+        print("Descrição:",self.cena_do_templo)
