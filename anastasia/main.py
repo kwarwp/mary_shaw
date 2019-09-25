7a539af92fb30122cb15aeef6532a2116aa25ed3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:07:30 -0300

    Saved anastasia/main.py @2019-09-25 15:07

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,25 +1,28 @@
 # mary_shaw.anastasia.main.py
-from _spy.vitollino.main import Cena
-IMAGEM_DO_TEMPLO = "https://i.imgur.com/VGAHhzQ.jpg"  
+
 """
     Jogo do Tesouro Inca
      
 """     
+from _spy.vitollino.main import Cena
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/VGAHhzQ.jpg"  
+
 __author__ = "Claudia L R Motta (claudiam@nce.ufrj.br)"
 
 
 
 class JogoTesouroInca:
     """ Representa o Jogo principal """
+    def __init__(self):
+        """Constroi a cena"""
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        
     def inicia(self):
         """Inicia a construção do Jogo"""
-        self.cena_do_templo = "Templo do tesouro Inca"
-        print("Descrição:", self.cena_do_templo)
-        
+        pass        
         
 if __name__ == "__main__": 
     jogo = JogoTesouroInca()
-    outro_jogo = JogoTesouroInca()
     jogo.inicia()
     
 
