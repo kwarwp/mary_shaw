a3734504026aa04d2fb112beba048f7131a12601
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:39:45 -0300

    Created libby/main.py @2019-09-25 14:39

---
+++ libby/main.py
@@ -0,0 +1,14 @@
+# mary_shaw.libby.main.py
+#http://supygirls.pythonanywhere.com/
+"""
+    Jogo do Tesouro Inca
+"""
+__author__ = "Carlos Augusto T. Alves"
+
+class JogoTesouroInca:
+   """ Representa o jogo principal """
+   def inicia(self):
+       """ Inicia a construção do jogo"""
+       self.cena_do_templo = "Templo do tesouro Inca"
+       print("Descrição:",self.cena_do_templo)
+   
