f00291e1e1c0723d223ef1ccb1c071ebbadfd160
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:44:07 -0300

    Created anastasia/__log__.py @2019-09-25 14:44

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 25 2019 11:44:14.429 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  """
+  ^
+IndentationError: unexpected indent
+'''},
