f0e71006c47a535e28fa4186c72ae6fdd8bbb7f4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:11:04 -0300

    Created lisa/__log__.py @2019-09-25 15:11

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 24 2019 19:20:44.264 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  """ Constroi a Cena"""
+  ^
+IndentationError: expected an indented block
+'''},
