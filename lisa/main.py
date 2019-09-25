bb33e27aa29acfd33d6d7f0036076333a89e6fbb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:03:47 -0300

    Saved lisa/main.py @2019-09-25 15:03

--- lisa/main.py
+++ lisa/main.py
@@ -1,10 +1,10 @@
 # mary_shaw.lisa.main.py
-from spy.vitollino.main import Cena
 """
     Título: Jogo do Tesouro Inca
 """
 __author__ = "Victor Azevedo (victorvaacs@gmail.com)"
 #PEP 8 diz que a primeira parte é a documentação
+from spy.vitollino.main import Cena
 IMAGEM_DO_TEMPLO = "https://i.imgur.com/hgBdcTi.jpg"
 #constantes em python são escritas em CAPSLOCK por isso IMAGEM_DO_TEMPLO é escrita assim pq ela é uma constante
 
