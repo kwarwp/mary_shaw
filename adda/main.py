c9b30941f024c9f05b2bfa642a8aa92bb4d4e251
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:16:01 -0300

    Saved adda/main.py @2019-09-25 15:16

--- adda/main.py
+++ adda/main.py
@@ -4,18 +4,23 @@
    Tesouro Inca
    
 """
+from _spy.vitollino.main import Cena
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
+
 __author__= "Marilia Campos Galvao"
 
 #camel case só se usa em nome de classe
 class JogoTesouroInca:
     """ Representa o Jogo principal """
+    def __init__(self):
+        """ Constroi a construção do jogo """
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        
     def inicia(self):
         """ Inicia a construção do jogo """
-        self.cena_do_templo = "Templo do tesouro Inca"
-        print("Descrição:",self.cena_do_templo, __name__)
+        self.cena_do_templo.vai
         
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
-    outro_jogo = JogoTesouroInca()
     jogo.inicia()
