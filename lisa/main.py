d0cf8c2c4d6a788a6492f2ce201fad954562adba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:02:59 -0300

    Saved lisa/main.py @2019-09-25 15:02

--- lisa/main.py
+++ lisa/main.py
@@ -1,9 +1,13 @@
 # mary_shaw.lisa.main.py
+from spy.vitollino.main import Cena
 """
     Título: Jogo do Tesouro Inca
 """
 __author__ = "Victor Azevedo (victorvaacs@gmail.com)"
 #PEP 8 diz que a primeira parte é a documentação
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/hgBdcTi.jpg"
+#constantes em python são escritas em CAPSLOCK por isso IMAGEM_DO_TEMPLO é escrita assim pq ela é uma constante
+
 
 class JogoTesouroInca:
 #kamelcase só usa em classes
@@ -16,5 +20,4 @@ class JogoTesouroInca:
         print("Descrição:", self.cena_do_templo)
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
-    outro_jogo = JogoTesouroInca()
     jogo.inicia()
