1b6d80e4b491c54dc036f68e05dcdfe69d6fa9d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:11:33 -0300

    Saved kathryn/main.py @2019-09-25 15:11

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,19 +1,27 @@
 # mary_shaw.kathryn.main.py
+# from adda.main import JogoTesouroInca as JogoMarilia
 """
     Tesouro Inca
 """
+from _spy.vitollino.main import Cena
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
+
 __author__ = "Pablo LV"
 
 
 class JogoTesouroInca:
     """ Representa o Jogo principal """
+    def __init__(self):
+        """ Constroi a cena"""
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        
     def inicia(self):
         """ Inicia a construção do Jogo """
-        self.cena_do_templo = "Templo do tesouro Inca"
-        print("Descrição:", self.cena_do_templo)
+        pass
         
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
-    outro_jogo = JogoTesouroInca()
     jogo.inicia()
+    
+    
