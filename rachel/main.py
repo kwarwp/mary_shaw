63153de1ea809148f24c0bc1317a68151ae8c538
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:39:30 -0300

    Saved rachel/main.py @2019-09-25 14:39

--- rachel/main.py
+++ rachel/main.py
@@ -6,8 +6,9 @@
 __author__ = "Andre R Vieira"
 
 
-
 class JogoTesouroInca:
     """ Representa o Jogo principal """
     def inicia(self):
+        """ Inicia a construção do Jogo """
         self.cena_do_templo = "Templo do tesouro Inca"
+        print("Descrição:", self.cena_do_templo)
