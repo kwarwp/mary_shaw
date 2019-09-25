bc4a42f970c290d716c390aa26831a129d5438cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:06:55 -0300

    Saved morgan/main.py @2019-09-25 15:06

--- morgan/main.py
+++ morgan/main.py
@@ -1,5 +1,7 @@
 # mary_shaw.morgan.main.py
 # http://supygirls.python.pythonanywhere.com/
+from _spy.vitollino.main import Cena
+IMAGE_DO_TEMPLO = "https://i.imgur.com/rY28mtM.jpg"
 
 """
     Jogo do Tesouro Inca
@@ -10,11 +12,17 @@ __author__ = "Rafael Ris-Ala (rafaelrisala@ufrj.br)"
 
 class JogoTesouroInca:
     """ Representa o Jogo Principal """
+    def __init__(self):
+        """Constroi a cena """
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        
     def inicia(self):
         """ Inicia a construção do jogo"""
-        self.cena_do_templo = "Templo do tesouro Inca"
+"""        self.cena_do_templo = "Templo do tesouro Inca"
         print("Descrição:", self.cena_do_templo)
-        
+"""
+        pass
+
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
     outro_jogo = JogoTesouroInca()
