ca34e8a90ec28d7307b32f0acad46150cadb40d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:46:06 -0300

    Created kathryn/__log__.py @2019-09-25 14:46

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 25 2019 11:45:02.513 GMt-0300 (hora estándar de Brasilia) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  self.cena_do_templo = "Templo de tesouro Inca"
+  ^
+IndentationError: expected an indented block
+'''},
