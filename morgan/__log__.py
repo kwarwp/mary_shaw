76e5a4c2800811182d8e0bb9ab51b14b73eefe46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:18:25 -0300

    Created morgan/__log__.py @2019-09-25 14:18

---
+++ morgan/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Wed Sep 25 2019 11:18:26.973 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    python
+NameError: name 'python' is not defined
+'''},
