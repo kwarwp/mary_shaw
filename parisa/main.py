4d0841e666c5153b041ae7f7532fe14835abd3aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:15:42 -0300

    Saved parisa/main.py @2019-09-25 15:15

--- parisa/main.py
+++ parisa/main.py
@@ -14,26 +14,25 @@ IMAGEM_DO_TEMPLO = "https://i.imgur.com/8puEH1k.jpg"   #constante em python é c
 __author__ = "Fernanda Prazeres (fprazeres@dcc.ufrj.br)"  
 #camelcase (nome com letra maiúscula e minúscula) ("letra de camelo") = só se usa em nome de classe
 
-class JogoTesouroInca:
+class JogoTesouroInca: #define classe
     """ Representa o Jogo principal """ #documentação = string apos 2 pontos, pode ser executado. 
     def __init__(self):   #classe tem um atributo 
         "Constroi a cena "
-        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO) # esse é o atributo
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO) # atributo
 
-    def inicia(self):
+    def inicia(self): #define comportamento
         """ Inicia a construção do Jogo """
         self.cena_do_templo.vai()
           
-          
-          #self.cena_do_templo = "Templo do Tesouro Inca"
-       # print("Descrição:",self.cena_do_templo,__name__)
-        
-        #PASS = comando que passa e não lê onde não tem comando, logo não dá erro.
+#self.cena_do_templo = "Templo do Tesouro Inca"
+# print("Descrição:",self.cena_do_templo,__name__)
+      
+#PASS = comando que passa e não lê onde não tem comando, logo não dá erro.
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca() 
     outro_jogo = JogoTesouroInca()
-    jogo.inicia()
+    jogo.inicia() #instancia a class
     
     
     
