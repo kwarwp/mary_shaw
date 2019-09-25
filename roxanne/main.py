c52d7ba1e8c508fa1ca8cff7789c36f30361ca06
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:54:37 -0300

    Saved roxanne/main.py @2019-09-25 14:54

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,4 +1,5 @@
 # mary_shaw.roxanne.main.py
+from adda.main import JogoTesouroInca as JogoMarilia
 """
     Tesouro Inca
 """
@@ -9,10 +10,12 @@ class JogoTesouroInca:
     """ Representa o Jogo principal """
     def inicia(self):
         """ Inicia a construção do Jogo """
-        self.cena_do_templo = "Templo do tesouro Inca"
-        print("Descrição:", self.cena_do_templo)
+        self.cena_do_templo = "Templo perdido do tesouro Inca"
+        print("Descrição:", self.cena_do_templo, __name__)
         
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
-    outro_jogo = JogoTesouroInca()
+    outro_jogo = JogoMarilia()
+    jogo.inicia()
+    outro_jogo.inicia()
