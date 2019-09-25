a1486092ccee04bd3f75b45b0baa0ce95e48f5b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 17:46:24 -0300

    Created roxanne/__author__.py @2019-09-25 17:46

---
+++ roxanne/__author__.py
@@ -0,0 +1,2 @@
+{
+"roxanne": {"author_nick": "T_Inca_Carlo", "author_name": "Labase - Disciplina de ProgramaÃ§Ã£o Orientada a Objetos - PPGI 2019-3", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
