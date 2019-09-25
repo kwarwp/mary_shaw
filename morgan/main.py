963b3195b0ae7ec5a2d71ea49a19ded8feac9b3e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:43:53 -0300

    Created morgan/main.py @2019-09-25 14:43

---
+++ morgan/main.py
@@ -0,0 +1,19 @@
+# mary_shaw.morgan.main.py
+# http://supygirls.python.pythonanywhere.com/
+
+"""
+    Jogo do Tesouro Inca
+
+"""
+
+__author__ = "Rafael Ris-Ala (rafaelrisala@ufrj.br)"
+
+class JogoTesouroInca:
+    """ Representa o Jogo Principal """
+    def inicia(self):
+        """ Inicia a construção do jogo"""
+        self.cena_do_templo = "Templo do tesouro Inca"
+        print("Descrição:", self.cena_do_templo)
+        
+if __name__ == "__main__":
+    jogo = JogoTesouroInca()
