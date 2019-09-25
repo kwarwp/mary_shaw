fbfab6ff7ad577b264075237dce312cd58977b28
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:39:32 -0300

    Created soraya/main.py @2019-09-25 14:39

---
+++ soraya/main.py
@@ -0,0 +1,13 @@
+# mary_shaw.soraya.main.py
+"""
+    Jogo do Tesouro Inca
+"""
+
+__author__ = "Sávyo V Morais (savyo.morais@labnet.nce.ufrj.br)"
+
+class JogoTesouroInca:
+    """ Representa o Jogo principal """
+    def inicia(self):
+        """ Inicia a construção do Jogo """
+        self.cena_do_templo  = "Templo do Tesouro Inca"
+        print("Descrição: ", self.cena_do_templo)
