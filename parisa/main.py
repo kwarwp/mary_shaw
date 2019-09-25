c346e46da0ad2b69caba945a8c41d992bb53f3f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:29:46 -0300

    Created parisa/main.py @2019-09-25 14:29

---
+++ parisa/main.py
@@ -0,0 +1,13 @@
+# mary_shaw.parisa.main.py
+#site: http://supygirls.pythonanywhere.com/
+
+"""
+     Jogo do Tesouro Inca
+
+"""
+__author__ = "Fernanda Prazeres (fprazeres@dcc.ufrj.br)"  
+
+class JogoTesouroInca:
+    """ Representa
+    
+
