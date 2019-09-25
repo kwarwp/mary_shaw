c48185a4f0585d8783a143607e668e2aa75e1432
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:11:22 -0300

    Saved lisa/__log__.py @2019-09-25 15:11

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Sep 24 2019 19:21:04.732 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  """Constroi a Cena"""
+  ^
+IndentationError: expected an indented block
+'''},
