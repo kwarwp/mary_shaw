af616d60f8aaaff877bb479e4ea09d3cff3a226f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:02:18 -0300

    Saved grace/main.py @2019-09-25 15:02

--- grace/main.py
+++ grace/main.py
@@ -1,8 +1,11 @@
 # mary_shaw.grace.main.py
+from spy.vitollino.main import Cena
 """
     Jogo do Tesouro Inca
 """
 __author__ = "Andre"
+
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/pq242D7.jpg"
 
 class JogoTesouroInca:
     """ Representa o jogo principal """
