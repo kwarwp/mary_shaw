eb5bc621441bd97f2da46a300d6c6c5a89ef77fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:39:36 -0300

    Created anastasia/main.py @2019-09-25 14:39

---
+++ anastasia/main.py
@@ -0,0 +1,17 @@
+ mary_shaw.anastasia.main.py
+
+"""
+    Jogo do Tesouro Inca
+     
+"""     
+__author__ = "Claudia L R Motta (claudiam@nce.ufrj.br)"
+
+
+class JogoTesouroInca:
+    """ Representa o Jogo principal """
+    def inicia(self):
+        """Inicia a construção do Jogo"""
+        self.cena_do_templo = "Templo do tesouro Inca"
+        print("Descrição:", self.cena_do_templo)
+
+    
