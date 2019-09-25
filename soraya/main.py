6669665461afb644f621b16173af2db77adeed75
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:07:32 -0300

    Saved soraya/main.py @2019-09-25 15:07

--- soraya/main.py
+++ soraya/main.py
@@ -13,13 +13,13 @@ class JogoTesouroInca:
     def __init__(self):
         """ Constroi a cena """
         
+        self.cena_do_templo  = Cena(IMAGEM_DO_TEMPLO)
+        
         
     def inicia(self):
         """ Inicia a construção do Jogo """
-        self.cena_do_templo  = "Templo do Tesouro Inca"
-        print("Descrição: ", self.cena_do_templo)
+        pass
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
-    outro_jogo = JogoTesouroInca()
     jogo.inicia()
