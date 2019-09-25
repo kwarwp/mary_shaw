658b3923d14f8d6ffccd1eff05e3547d8da0a7f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 11:44:04 -0300

    Created soraya/__log__.py @2019-09-25 14:44

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 25 2019 11:44:01.956 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  if __name__ = "__main__":
+               ^
+SyntaxError: invalid syntax
+'''},
