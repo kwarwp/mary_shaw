27823e54551210c3a23775a8aa4610fd1ca2cd62
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 12:10:33 -0300

    Saved rachel/main.py @2019-09-25 15:10

--- rachel/main.py
+++ rachel/main.py
@@ -20,7 +20,7 @@ class JogoTesouroInca:
         
     def inicia(self):
         """ Inicia a construção do Jogo """
-        pass
+        self.cena_do_templo.vai()
         
         
 if __name__ == "__main__":
