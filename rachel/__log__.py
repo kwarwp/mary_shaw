99f322b5c14eb200e271c8c9c845fe67fe9e6623
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:15:42 -0300

    Saved rachel/__log__.py @2019-10-02 15:15

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -112,3 +112,21 @@ Exception: <TypeError: $B.imported[_mod_name] is null>
     self.tumba = Tumba()
 TypeError: __init__() missing 1 positional argument: acampamento
 '''},
+{'date': 'Wed Oct 02 2019 12:15:42.247 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 206
+    jogo = JogoTesouroInca()
+  module <module> line 177
+    self.tumba = Tumba(acampamento)
+NameError: name 'acampamento' is not defined
+'''},
