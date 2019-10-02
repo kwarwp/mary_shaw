15851570e42f58e1fd8a2d5bf8b89e1f015ec7a2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:51:56 -0300

    Saved roxanne/__log__.py @2019-10-02 14:51

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -142,3 +142,23 @@ AttributeError: 'JogoTesouroInca' object has no attribute 'cena_do_templo'
     self.tumba = [Tesouro(pedras+1) for pedras in range(4)]
 NameError: name 'Tesouro' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 11:51:55.390 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    jogo = JogoTesouroInca()
+  module <module> line 70
+    self.tumba = Tumba()
+  module <module> line 45
+    self.tumba = [TesouroS(pedras+1) for pedras in range(4)]
+NameError: name 'TesouroS' is not defined
+'''},
