37b7b9021a1838cad1eb2e5f34753d1eae518f3e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 10:48:03 -0300

    Saved tracy/__log__.py @2019-10-02 13:48

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -101,3 +101,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Oct 02 2019 10:48:04.478 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 58
+  jogo = JogoTesouroInca()
+  ^
+IndentationError: expected an indented block
+'''},
