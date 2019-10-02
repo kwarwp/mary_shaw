7c1b133057b00a26ea1b84f7ee997214981d80dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 10:12:46 -0300

    Created julia/__log__.py @2019-10-02 13:12

---
+++ julia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 02 2019 10:12:43.871 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  class JogoTesouroInca:
+  ^
+IndentationError: expected an indented block
+'''},
