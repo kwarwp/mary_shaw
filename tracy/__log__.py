55c511692c985a4a972be9330b62f7fdcb415bb7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:18:09 -0300

    Saved tracy/__log__.py @2019-10-02 14:18

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -155,3 +155,25 @@ AttributeError: 'Acampamento' object has no attribute 'tesouro'
     self.tesouro += valor      
 AttributeError: 'Acampamento' object has no attribute 'tesouro'
 '''},
+{'date': 'Wed Oct 02 2019 11:18:09.992 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    jogo = JogoTesouroInca()
+  module <module> line 69
+    self.acampamento = Acampamento(ACAMPAMENTO)
+  module <module> line 42
+    self.pedra =Elemento(TURQUESA, cena=self)
+  module elemento.main line 40
+    self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
+AttributeError: 'Acampamento' object has no attribute 'nome'
+'''},
