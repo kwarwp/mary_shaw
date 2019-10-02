7d4982776ca26a9d242b66440ee6d241cd19005f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:18:14 -0300

    Saved rachel/__log__.py @2019-10-02 14:18

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -31,3 +31,23 @@ NameError: name 'JogoTesouroinca' is not defined
     from adda.main.py import JogoTesouroInca as JogoMarilia
 ImportError: adda.main
 '''},
+{'date': 'Wed Oct 02 2019 11:18:13.521 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 170
+    jogo = JogoTesouroInca()
+  module <module> line 142
+    self.acampamento = Acampamento(ACAMPAMENTO)
+  module <module> line 83
+    self.pedra = Elemento(TURQUESA, cena=self)
+NameError: name 'Elemento' is not defined
+'''},
