1699ef518202d3123340c9a22307f54c9b60d8a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 10:08:09 -0300

    Saved roxanne/main.py @2019-10-02 13:08

--- roxanne/main.py
+++ roxanne/main.py
@@ -15,29 +15,9 @@ TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
 
 TUMBA = [COBRA, MUMIA] + [TESOURO]*3
 
-class Jogador(Cena):
-    def continua(self):
-        INVENTARIO.bota("turquesa", TURQUESA)
+class Jogador():
+    
 
-class Camara(Cena):
-    """ Uma das Camaras """
-    def __init__(self, cena=MUMIA, jogador=None):
-        """ Constroi a cena"""
-        camara = self
-        class ProximaCamara(Cena):
-            def vai(self):
-                jogador.continua()
-                proxima = choice(TUMBA)
-                Camara(proxima, jogador).vai()
-                #super().vai()
-        self._proxima = ProximaCamara()
-        super().__init__(cena, direita=self._proxima)
-    @property
-    def direita(self):
-        pass
-    @direita.setter
-    def set_direita(self, camara):
-        self._proxima = camara
 class JogoTesouroInca:
     """ Representa o Jogo principal """
     def __init__(self):
