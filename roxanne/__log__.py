f6abcdb1c4a4e2c5f69c64075db3e3fbb4025f7e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:52:22 -0300

    Saved roxanne/__log__.py @2019-10-02 14:52

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -162,3 +162,25 @@ NameError: name 'Tesouro' is not defined
     self.tumba = [TesouroS(pedras+1) for pedras in range(4)]
 NameError: name 'TesouroS' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 11:52:21.241 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    jogo = JogoTesouroInca()
+  module <module> line 70
+    self.tumba = Tumba()
+  module <module> line 45
+    self.tumba = [Tesouros(pedras+1) for pedras in range(4)]
+  module <module> line 34
+    super().__init__(cena)
+NameError: name 'cena' is not defined
+'''},
