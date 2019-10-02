fb41a1dbd8979b37bc1baec33c0c4e43b1a508b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 10:32:04 -0300

    Saved kathryn/main.py @2019-10-02 13:32

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,16 +1,35 @@
 # mary_shaw.kathryn.main.py
 # from adda.main import JogoTesouroInca as JogoMarilia
 """
- Tesouro Inca
+    Tesouro Inca
 """
-from _spy.vitollino.main import Cena
-IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
-
+from _spy.vitollino.main import Cena, INVENTARIO, Elemento
+from random import choice
 
 __author__ = "Pablo LV"
 
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
+MUMIA = "https://i.imgur.com/YGV3CPB.jpg"
+COBRA = "https://i.imgur.com/IeC0gBj.jpg"
+TESOURO = "https://i.imgur.com/xkBdHCE.jpg"
+TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
+
+TUMBA = [COBRA, MUMIA] + [TESOURO]*3
+
+#-------------------------------------------------------------------------
+class Jogador():
+    """ Um explorador em busca de tesouros """
+    def __init__(self):
+        """ Inicia com tesouro """
+
+#-------------------------------------------------------------------------
+class Tumba():
+    """ Um complexo de camaras secretas sob o templo """
+    def __init__(self):
+        """ Inicia o complexo de camaras """
 
 
+#-------------------------------------------------------------------------
 class JogoTesouroInca:
     """ Representa o Jogo principal """
     def __init__(self):
@@ -19,13 +38,15 @@ class JogoTesouroInca:
         
     def inicia(self):
         """ Inicia a construção do Jogo """
-        pass
-
-
+        self.cena_do_templo.vai()
+        
+#-------------------------------------------------------------------------
         
 if __name__ == "__main__":
     jogo = JogoTesouroInca()
-    #jogo.inicia()
-    print(JogoTesouroInca.cena_do_templo(),JogoTesouroInca(),JogoTesouroInca())
+    jogo.inicia()
+    print(JogoTesouroInca,JogoTesouroInca())
+
+
 
 
