dfe266f24b9dd663036530f9fbc076c840f7c928
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:27:33 -0300

    Saved tracy/main.py @2019-10-02 15:27

--- tracy/main.py
+++ tracy/main.py
@@ -3,7 +3,7 @@
 """
     Tesouro Inca
 """
-from _spy.vitollino.main import Cena, INVENTARIO, Elemento
+from _spy.vitollino.main import Cena, INVENTARIO
 from elemento.main import Elemento
 from random import choice
 
@@ -19,7 +19,7 @@ TURQUESA = "https://i.imgur.com/8WDBJM3.png"
 ACAMPAMENTO = "https://i.imgur.com/BoPPjLb.jpg"
 
 TUMBA = [COBRA, MUMIA] + [TESOURO]*3
-
+'''
 class PedrasPreciosas():
     """ Um Conjunto de pedras que se organizam por valor """
     def __init__(self, valor):
@@ -39,23 +39,32 @@ class PedrasPreciosas():
         """ Aumenta o tesouro com valor equivalente de turquesas 
             :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
         """   
-        self.tesouro += valor      
+        self.tesouro += valor      '''
 
 class Jogador():
     """ Um explorador em busca de tesouros """
     def __init__(self):
         """ Inicia com tesouro """
+        self.tesouro = 0
 
-
+    def ganho(self, valor):
+        """ Aumenta o tesouro com valor equivalente de turquesas 
+            :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
+        """   
+        self.tesouro += valor
+        INVENTARIO.bota("turquesa", TURQUESA)
+        
 class Tesouros(Cena):
     """ Camaras secretas contendo tesouros """
-    def __init__(self, quantas_pedras=4, acampamento=None):
+    def __init__(self, quantas_pedras=4, acampamento=None, eu=None):
         """ Inicia a camara contendo umas pedras
             :param int quantas_pedras: numero de pedras nesta camara
         """
         class ProximaCamara:
             def vai(self):
-                Tesouros(choice(range(1,5)), acampamento).vai()
+                pedras_na_camara = choice(range(1,5))
+                Tesouros(pedras_na_camara, acampamento, eu).vai()
+                eu.ganho(pedras_na_camara)
         self.tesouro = quantas_pedras
         super().__init__(TESOURO, esquerda=acampamento, direita=ProximaCamara())
         #self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
@@ -66,9 +75,9 @@ class Tesouros(Cena):
 
 class Tumba():
     """ Um complexo de camaras secretas sob o templo """
-    def __init__(self, acampamento):
+    def __init__(self, acampamento, eu):
         """ Inicia o complexo de camaras """
-        self.tumba = [Tesouros(pedras+1, acampamento) for pedras in range(4)]
+        self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
         self.inicial = choice(self.tumba)
 
 
@@ -93,7 +102,8 @@ class JogoTesouroInca:
     def __init__(self):
         """ Constroi a cena"""
         self.acampamento = Acampamento(ACAMPAMENTO)
-        self.tumba = Tumba(self.acampamento)
+        self.eu = Jogador()
+        self.tumba = Tumba(self.acampamento, self.eu)
         self.cena_do_templo = Cena(
         IMAGEM_DO_TEMPLO, self.acampamento, direita=self.tumba.inicial)
         self.acampamento.direita = self.cena_do_templo
