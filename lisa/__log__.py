b22917de20a05b775101d16ee07f01b971c6d42e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:56:45 -0300

    Saved lisa/__log__.py @2019-10-02 14:56

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -61,3 +61,23 @@ AttributeError: 'str' object has no attribute 'vai'
     super().__init__(cena)
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 11:56:45.294 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 91
+    jogo = JogoTesouroInca()
+  module <module> line 79
+    self.tmba = Tumba()
+  module <module> line 48
+    self.tumba = [Tesouro(pedras+1) for pedras in range(4)]
+NameError: name 'Tesouro' is not defined
+'''},
