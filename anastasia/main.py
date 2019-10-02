4a0bb1ddcd150f31ed68dec339a80c76e3101faf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:23:10 -0300

    Saved anastasia/main.py @2019-10-02 14:23

--- anastasia/main.py
+++ anastasia/main.py
@@ -68,6 +68,6 @@ if __name__ == "__main__":
     jogo = JogoTesouroInca()
     #jogo.inicia()
     #print(JogoTesouroInca,JogoTesouroInca(), JogoTesouroInca().cena_dotemplo())
-    print(help(PedrasPreciosas.aumenta)
+    print(help(PedrasPreciosas.aumenta))
 
     
