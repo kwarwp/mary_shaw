31000a3e975c2dcfa86fd228d7dbb38d626e2f1d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:22:07 -0300

    Saved rachel/__log__.py @2019-10-02 15:22

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -134,3 +134,23 @@ NameError: name 'acampamento' is not defined
 'error': '''
 Exception: <TypeError: $B.imported[_mod_name] is null>
 '''},
+{'date': 'Wed Oct 02 2019 12:22:07.320 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 206
+    jogo = JogoTesouroInca()
+  module <module> line 177
+    self.acampamento = Acampamento(ACAMPAMENTO)
+  module <module> line 92
+    self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
+TypeError: 'module' object is not callable
+'''},
