12a3643e51aa76d835b3295bc167fe796742b3ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:21:26 -0300

    Saved anastasia/main.py @2019-10-02 14:21

--- anastasia/main.py
+++ anastasia/main.py
@@ -13,6 +13,7 @@ MUMIA = "https://i.imgur.com/SoMZVzD.jpg"
 COBRA = "https://i.imgur.com/IeC0gBj.jpg"
 TESOURO = "https://i.imgur.com/xkBdHCE.jpg"
 TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
+ACAMPAMENTO = "https://i.imgur.com/MEl27uM.png"
 
 TUMBA = [COBRA, MUMIA] + [TESOURO]*3
 
@@ -28,11 +29,26 @@ class Tumba():
         """ Inicia o complexo de camaras """
 
 
+class Acampamento(Cena):
+    """ Um lugar seguro e quentinho para admirar seus ganhos """
+    def __init__(self, cena):
+        """ Cria a cena de um acampamento com tesouros """
+        self.tesouro = 0
+        super().__init__(cena)
+        self.pedra = Elemento(TURQUESA, cena=self)
+
+    def ganho(self, valor):
+        """ aumenta o tesouro com um valor de pedras """
+        self.tesouro += valor
+
+
 class JogoTesouroInca:
     """ Representa o Jogo principal """
     def __init__(self):
         """ Constroi a cena"""
-        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        self.acampamento = Acampamento(ACAMPAMENTO)
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO, self.acampamento)
+        self.acampamento.direita = self.cena_do_templo
         
     def inicia(self):
         """ Inicia a construção do Jogo """
@@ -43,5 +59,14 @@ if __name__ == "__main__":
     jogo = JogoTesouroInca()
     jogo.inicia()
     print(JogoTesouroInca,JogoTesouroInca())
+
+        
+        
+        
+if __name__ == "__main__":
+    jogo = JogoTesouroInca()
+    #jogo.inicia()
+    #print(JogoTesouroInca,JogoTesouroInca(), JogoTesouroInca().cena_dotemplo())
+    print(help(PedrasPreciosas.aumenta)
 
     
