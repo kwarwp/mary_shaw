eb1c2bd28d0682704f8639ce23cf21f6d19a1eae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:59:31 -0300

    Saved lisa/__log__.py @2019-10-02 14:59

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -81,3 +81,25 @@ NameError: name 'cena' is not defined
     self.tumba = [Tesouro(pedras+1) for pedras in range(4)]
 NameError: name 'Tesouro' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 11:59:30.457 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 92
+    jogo = JogoTesouroInca()
+  module <module> line 80
+    self.tmba = Tumba()
+  module <module> line 49
+    self.tumba = [Tesouros(pedras+1) for pedras in range(4)]
+  module <module> line 39
+    super().__init__(TESOURO, direita=proxima_camara)
+NameError: name 'proxima_camara' is not defined
+'''},
