f4058f9fedce00712119f45e5ba7596aa4ddb26c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 10:12:39 -0300

    Created sarah/main.py @2019-10-02 13:12

---
+++ sarah/main.py
@@ -0,0 +1,46 @@
+# mary_shaw.sarah.main.py
+# from adda.main import JogoTesouroInca as JogoMarilia
+"""
+    Tesouro Inca
+"""
+from _spy.vitollino.main import Cena, INVENTARIO, Elemento
+from random import choice
+
+__author__ = "Carlo"
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
+MUMIA = "https://i.imgur.com/YGV3CPB.jpg"
+COBRA = "https://i.imgur.com/IeC0gBj.jpg"
+TESOURO = "https://i.imgur.com/xkBdHCE.jpg"
+TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
+
+TUMBA = [COBRA, MUMIA] + [TESOURO]*3
+
+class Jogador():
+    """ Um explorador em busca de tesouros """
+    def __init__(self):
+        """ Inicia com tesouro """
+
+
+class Tumba():
+    """ Um complexo de camaras secretas sob o templo """
+    def __init__(self):
+        """ Inicia o complexo de camaras """
+
+
+class JogoTesouroInca:
+    """ Representa o Jogo principal """
+    def __init__(self):
+        """ Constroi a cena"""
+        camara = Camara(jogador=Jogador())
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO, direita=camara)
+        camara.direita = Camara(COBRA, Jogador())
+        
+    def inicia(self):
+        """ Inicia a construção do Jogo """
+        self.cena_do_templo.vai()
+        
+        
+if __name__ == "__main__":
+    jogo = JogoTesouroInca()
+    jogo.inicia()
+    #print(help(JogoTesouroInca.inicia))
