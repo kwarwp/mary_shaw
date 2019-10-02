8d5e8c53606905afe906633bb36d7614f72e263c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:25:49 -0300

    Saved tracy/__log__.py @2019-10-02 15:25

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -413,3 +413,21 @@ NameError: name 'tesouros' is not defined
     def __init__(self, quantas_pedras=4, acampamento=None, eu=none):
 NameError: name 'none' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 12:25:50.388 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    class Tesouros(Cena):
+  module <module> line 59
+    def __init__(self, quantas_pedras=4, acampamento=None, eu=none):
+NameError: name 'none' is not defined
+'''},
