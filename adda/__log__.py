81d7012ea1b364030c1913b4be9d7d33f103de3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:27:59 -0300

    Created adda/__log__.py @2019-10-02 15:27

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 02 2019 12:27:57.566 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 50
+  Tesouro(choice(range)(1,4))).vai()
+                             ^
+SyntaxError: invalid syntax
+'''},
