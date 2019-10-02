603849cc3849e65cb2baf372639cb7923e149616
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:14:09 -0300

    Saved tracy/main.py @2019-10-02 14:14

--- tracy/main.py
+++ tracy/main.py
@@ -4,6 +4,7 @@
     Tesouro Inca
 """
 from _spy.vitollino.main import Cena, INVENTARIO, Elemento
+from elemento.main import Elemento
 from random import choice
 
 __author__ = "Lorena"
@@ -15,6 +16,7 @@ FOGO = "https://i.imgur.com/LGlVkUM.png"
 ARANHA = "https://i.imgur.com/DMAU5DF.gif"
 PEDREIRA = "https://i.imgur.com/4JPWbQg.png"
 TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
+ACAMPAMENTO = "https://i.imgur.com/BoPPjLb.jpg"
 
 TUMBA = [COBRA, MUMIA] + [TESOURO]*3
 
@@ -29,7 +31,22 @@ class PedrasPreciosas():
     def aumenta(self, valor):
         """ Aumenta o tesouro com valor equivalente de turquesas 
             :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
-        """        
+        """   
+        ...
+        
+class Acampamento(Cena):
+    """ Acampamento é um lugar seguro e quentinho para admirar os seus ganhos """
+    def __init__(self, cena):
+        """ Cria a cena de um acampmanto com tesouro vazio """
+        self.tesouro = 0
+        super().__init__(cena)
+        
+        
+    def __init__(self, valor):
+        """ Aumenta o tesouro com valor equivalente de turquesas 
+            :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
+        """   
+        self.tesouro += valor      
 
 class Jogador():
     """ Um explorador em busca de tesouros """
@@ -47,7 +64,10 @@ class JogoTesouroInca:
     """ Representa o Jogo principal """
     def __init__(self):
         """ Constroi a cena"""
-        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        #self.acampamento = Cena(ACAMPAMENTO)
+        self.acampamento = Acampamento(ACAMPAMENTO)
+        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO, self.acampamento)
+        self.acampamento.direita = self.cena_do_templo
         
     def inicia(self):
         """ Inicia a construção do Jogo """
@@ -55,7 +75,7 @@ class JogoTesouroInca:
         
         
 if __name__ == "__main__":
-jogo = JogoTesouroInca()
-#jogo.inicia()
-#print(help(JogoTesouroInca.inicia, JogoTesouroInca.inicia())
-print(help(PedrasPreciosas))
+    jogo = JogoTesouroInca()
+    jogo.inicia()
+    #print(help(JogoTesouroInca.inicia, JogoTesouroInca.inicia())
+    #print(help(Cena))
