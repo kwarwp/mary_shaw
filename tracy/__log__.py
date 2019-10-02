37f65ca145e00daad5581443e61913fa465eaa8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 11:46:13 -0300

    Saved tracy/__log__.py @2019-10-02 14:46

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -234,3 +234,10 @@ SyntaxError: invalid syntax
                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 02 2019 11:46:14.527 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 60
+  self.pedras = [Elemento( TURQUESA, x=50+50*pedra, y=250 w=40, h=40, cena=self) for pedra in range(self.tesouro)]
+                                                           ^
+SyntaxError: invalid syntax
+'''},
