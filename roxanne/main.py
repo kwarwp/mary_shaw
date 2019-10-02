f58d6683d63ede0cba9e789deac468bfbf175be1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:29:45 -0300

    Saved roxanne/main.py @2019-10-02 15:29

--- roxanne/main.py
+++ roxanne/main.py
@@ -29,7 +29,7 @@ class Jogador():
             :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
         """   
         self.tesouro += valor
-        INVENTARIO.bota("turquesa", TURQUESA)
+        [INVENTARIO.bota("turquesa", TURQUESA) for _ in range(valor)]
 
 
 
