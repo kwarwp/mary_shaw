ec2c2ec0b515851cf756eb122acef08dc2ba7ac9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 12:35:44 -0300

    Saved adda/__log__.py @2019-10-02 15:35

--- adda/__log__.py
+++ adda/__log__.py
@@ -40,3 +40,23 @@ IndentationError: unexpected indent
     self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
 TypeError: __init__() takes 2 positional argument but more were given
 '''},
+{'date': 'Wed Oct 02 2019 12:35:43.293 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 100
+    jogo = JogoTesouroInca()
+  module <module> line 89
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 63
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+TypeError: __init__() takes 2 positional argument but more were given
+'''},
