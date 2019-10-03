ab537fe77bb7acc2fd39464201eab40ef724e0b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-03 11:36:17 -0300

    Saved rachel/__log__.py @2019-10-03 14:36

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -210,3 +210,19 @@ Exception: <TypeError: $B.imported[_mod_name] is null>
     def __init__(self, img="", vai=None, style=NDCT, tit="", alt="",
 NameError: name 'NDCT' is not defined
 '''},
+{'date': 'Thu Oct 03 2019 11:36:20.397 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    from _spy.vitollino.main import Cena, INVENTARIO
+ImportError: cannot import name 'Cena'
+'''},
