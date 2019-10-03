76631c2648902e37254deb4e8fcfcb1ed2da3b3d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-03 11:38:00 -0300

    Saved rachel/__log__.py @2019-10-03 14:38

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -242,3 +242,21 @@ ImportError: cannot import name 'Cena'
     from _spy.vitollino.main import Cena, INVENTARIO
 ImportError: cannot import name 'Cena'
 '''},
+{'date': 'Thu Oct 03 2019 11:38:03.760 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    from elemento.main import Elemento
+  module elemento.main line 3
+    from _spy.vitollino.main import Cena, Texto, INVENTARIO, STYLE, PSTYLE, EIMGSTY, Sala
+ImportError: cannot import name 'Cena'
+'''},
