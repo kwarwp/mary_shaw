d2f9d0bccc19c2b9ae616eadf0fcffab6cd1c7f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:55:01 -0300

    Saved tracy/__log__.py @2019-10-08 03:55

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -846,3 +846,21 @@ TypeError: __init__() missing 2 positional arguments: cena,cenarios
     self.cena_do_templo = Cena(
 AttributeError: 'JogoTesouroInca' object has no attribute 'self'
 '''},
+{'date': 'Tue Oct 08 2019 00:54:41.141 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    jogo = JogoTesouroInca()
+  module <module> line 122
+    self.tumba = Tumba(self.acampamento, self.eu)
+TypeError: __init__() missing 1 positional argument: cenario
+'''},
