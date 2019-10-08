5b804afa2dbcb8f8fe20c7c9730d5b82043d276f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:56:07 -0300

    Saved tracy/__log__.py @2019-10-08 03:56

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -904,3 +904,23 @@ NameError: name 'Cenarios' is not defined
     self.cenario = [Cenarios(CENARIO+1, x=50, y=250, w=40, h=40, cena=self)  for cenario in
 NameError: name 'Cenarios' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 00:55:47.56 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    jogo = JogoTesouroInca()
+  module <module> line 120
+    self.cenario = Cenario(CENARIO)
+  module <module> line 110
+    self.cenario = [Cenarios(CENARIO+1, x=50, y=250, w=40, h=40, cena=self)  for cenario in
+NameError: name 'Cenarios' is not defined
+'''},
