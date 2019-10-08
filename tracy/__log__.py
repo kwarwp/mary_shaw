b0996c8d6fdb1fa9a552b0496ab2172c3e19004c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:45:28 -0300

    Saved tracy/__log__.py @2019-10-08 03:45

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -720,3 +720,21 @@ NameError: name 'Cenarios' is not defined
     self.cenario = Cenario(CENARIO)
 TypeError: __init__() missing 1 positional argument: Cenarios
 '''},
+{'date': 'Tue Oct 08 2019 00:45:08.406 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 134
+    jogo = JogoTesouroInca()
+  module <module> line 121
+    self.cenario = Cenarios(CENARIO)
+NameError: name 'Cenarios' is not defined
+'''},
