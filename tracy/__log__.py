e1a9632bbe05890c03c7c5a39605b4a383c1b600
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:25:46 -0300

    Saved tracy/__log__.py @2019-10-08 03:25

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -539,3 +539,21 @@ AttributeError: 'JogoTesouroInca' object has no attribute 'CENARIO'
     self.cena_do_templo = Cena(
 TypeError: 'list' object cannot be interpreted as an integer
 '''},
+{'date': 'Tue Oct 08 2019 00:25:25.890 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 102
+    class Cenario(Cena):
+  module <module> line 104
+    self.cenario = Cenario(IMAGEM_DO_TEMPLO, IMAGEM_DO_TEMPLO2, IMAGEM_DO_TEMPLO3, IMAGEM_DO_TEMPLO4)
+NameError: name 'Cenario' is not defined
+'''},
