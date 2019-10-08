b75d2368de4d4ed6eaea50f97ac8744f0fe33cb8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:08:37 -0300

    Saved tracy/__log__.py @2019-10-08 03:08

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -503,3 +503,21 @@ AttributeError: 'JogoTesouroInca' object has no attribute 'IMAGEM_DO_TEMPLO'
     CENARIOMUDA = range(CENARIO)
 TypeError: 'list' object cannot be interpreted as an integer
 '''},
+{'date': 'Tue Oct 08 2019 00:08:17.703 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 121
+    jogo = JogoTesouroInca()
+  module <module> line 107
+    CENARIOMUDA = range(self.CENARIO)
+AttributeError: 'JogoTesouroInca' object has no attribute 'CENARIO'
+'''},
