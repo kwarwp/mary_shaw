6d0988455118e73bb6b3d0f6a53c72536afd4dab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:43:41 -0300

    Saved tracy/__log__.py @2019-10-08 03:43

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -682,3 +682,23 @@ TypeError: __init__() missing 1 positional argument: cena
     self.cenario = Cenario(self.mudacenario)
 AttributeError: 'JogoTesouroInca' object has no attribute 'mudacenario'
 '''},
+{'date': 'Tue Oct 08 2019 00:43:21.311 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 134
+    jogo = JogoTesouroInca()
+  module <module> line 121
+    self.cenario = Cenario(CENARIO)
+  module <module> line 109
+    self.cenario = Cenarios(  CENARIO, x=50, y=250, w=40, h=40, cena=self)  
+NameError: name 'Cenarios' is not defined
+'''},
