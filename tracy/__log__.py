5ce4bd74c171114ecbde98c7acea4cf794ee918e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 00:26:12 -0300

    Saved tracy/__log__.py @2019-10-08 03:26

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -557,3 +557,21 @@ TypeError: 'list' object cannot be interpreted as an integer
     self.cenario = Cenario(IMAGEM_DO_TEMPLO, IMAGEM_DO_TEMPLO2, IMAGEM_DO_TEMPLO3, IMAGEM_DO_TEMPLO4)
 NameError: name 'Cenario' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 00:25:51.996 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 102
+    class Cenario(cenario):
+  module <module> line 104
+    self.cenario = Cenario(IMAGEM_DO_TEMPLO, IMAGEM_DO_TEMPLO2, IMAGEM_DO_TEMPLO3, IMAGEM_DO_TEMPLO4)
+NameError: name 'Cenario' is not defined
+'''},
