a30ceb75f457d8ec18a4119052d13fe402dd12b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 10:54:14 -0300

    Saved adda/main.py @2019-10-09 13:54

--- adda/main.py
+++ adda/main.py
@@ -28,34 +28,36 @@ class Jogador():
     """ Um explorador em busca de tesouros """
     def __init__(self):
         """ Inicia com tesouro """
-        self.tesouro = 0 
-        
-        
+        self.tesouro = 0
+    
     def ganho(self, valor):
-        """ aumenta o tesouro com valor equivalente de turquesas
-           :param valor: valor acrescentar ao tesouro em número de turnos
-        """
+        """ Aumenta o tesouro com valor equivalente de turquesas 
+            :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
+        """   
         self.tesouro += valor
-        INVENTARIO.bota("turquesa", TURQUESA)
+        [INVENTARIO.bota("turquesa", TURQUESA) for _ in range(valor)]
+
 
 
 class Tesouros(Cena):
     """ Camaras secretas contendo tesouros """
-    def __init__(self, quantas_pedras=4):
+    def __init__(self, quantas_pedras=4, acampamento=None, eu=None):
         """ Inicia a camara contendo umas pedras
             :param int quantas_pedras: numero de pedras nesta camara
         """
         class ProximaCamara:
             def vai(self):
                 pedras_na_camara = choice(range(1,5))
-                Tesouros(pedras_na_camara, acampamento, eu).vai()
+                Tesouros(0, acampamento, eu).vai()
                 eu.ganho(pedras_na_camara)
         self.tesouro = quantas_pedras
         super().__init__(TESOURO, esquerda=acampamento, direita=ProximaCamara())
+        #self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
         self.pedras = [Elemento(
              TURQUESA, x=50+50*pedra, y=250, w=40, h=40, cena=self) for pedra in
              range(self.tesouro)]
-             
+
+
 class Tumba():
     """ Um complexo de camaras secretas sob o templo """
     def __init__(self, acampamento, eu):
@@ -64,48 +66,35 @@ class Tumba():
         self.inicial = choice(self.tumba)
 
 
-
 class Acampamento(Cena):
-    """ um lugar seguro e quentinho para admirar seus ganhos """
+    """ Um lugar seguro e quentinho para admirar seus ganhos """
     def __init__(self, cena):
-        """ Inicia com tesouro vazio """
-        self.tesouro = 0
+        """ Cria a cena de um acampamento com tesouros """
+        self.tesouro = 4
         super().__init__(cena)
-        self.pedra = Elemento(TURQUESA, cena=self)
-        
+        self.pedras = [Elemento(
+             TURQUESA, x=50+50*pedra, y=250, w=40, h=40, cena=self) for pedra in
+             range(self.tesouro)]
+
     def ganho(self, valor):
-        """ aumenta o tesouro com valor equivalente de turquesas
-           :param valor: valor acrescentar ao tesouro em número de turnos
-        """
+        """ aumenta o tesouro com um valor de pedras """
         self.tesouro += valor
 
 
-class PedrasPreciosas:
-    """ Um conjunto de pedras que se organizam por valor """
-    def __init__(self, valor):
-        """ Inicia com valor equivalente de turquesas
-           :param valor: valor do tesouro em número de turquesas.
-        """
-        ...
-
-    def aumenta(self, valor):
-        """ aumenta o tesouro com valor equivalente de turquesas
-           :param valor: valor a acrescentar ao tesouro em número de turnos
-        """
-        ...
-        
-        
-   #camel case só se usa em nome de classe
 class JogoTesouroInca:
     """ Representa o Jogo principal """
     def __init__(self):
-        """ Constroi a cena """
+        """ Constroi a cena"""
+        INVENTARIO.inicia()
         self.acampamento = Acampamento(ACAMPAMENTO)
-        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO)
+        self.eu = Jogador()
+        self.tumba = Tumba(self.acampamento, self.eu)
+        self.cena_do_templo = Cena(
+        IMAGEM_DO_TEMPLO, self.acampamento, direita=self.tumba.inicial)
         self.acampamento.direita = self.cena_do_templo
-         
+        
     def inicia(self):
-        """ Inicia a construção do jogo """
+        """ Inicia a construção do Jogo """
         self.cena_do_templo.vai()
         
         
