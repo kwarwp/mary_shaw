88da250961caea5c801d388be0a28553a2a9e4f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 12:28:54 -0300

    Saved tracy/__log__.py @2019-10-09 15:28

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -952,3 +952,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Oct 09 2019 12:28:53.937 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 104
+  """  def esvazia_camara(self):
+  ^
+IndentationError: unexpected indent
+'''},
