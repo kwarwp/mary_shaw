34c868a215fce56f18cea42fdfa783d0a7ff14a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 18:30:16 -0300

    Saved rachel/__log__.py @2019-10-09 21:30

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -694,3 +694,28 @@ Traceback (most recent call last):
     self.origem = 50 + 50 * tipos_de_pedras['ouro']
 NameError: name 'tipos_de_pedras' is not defined
 '''},
+{'date': 'Wed Oct 09 2019 18:30:17.602 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''{'ouro': 0, 'obsidiana': 0, 'turquesa': 1}
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 182
+    jogo = JogoTesouroInca()
+  module <module> line 171
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 144
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 132
+    self.pedras = PedrasPreciosas(quantas_pedras=self.tesouro)
+  module <module> line 67
+    self.origem = 50 + 50 * tipos_de_pedras['ouro']
+NameError: name 'tipos_de_pedras' is not defined
+'''},
