8f6062c3014a31b88b0a187ab0ade7dbaa7e1dbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 12:27:14 -0300

    Saved tracy/main.py @2019-10-09 15:27

--- tracy/main.py
+++ tracy/main.py
@@ -89,13 +89,14 @@ class Tesouros(Cena):
         class ProximaCamara:
             def vai(self):
                 self.pedras = pedras_na_camara = choice(range(1,5))
-                camara = Tesouros(pedras_na_camara, acampamento, eu)
+                self.c = camara = Tesouros(pedras_na_camara, acampamento, eu)
                 camara.vai()
                 Texto(camara, 
                       "Você encontra um tesouro!",
                       foi=self.pega_tesouro).vai()
             def pega_tesouro(self):
                 eu.ganho(self.pedras)
+                self.c.esvazia_camara()
         self.tesouro = quantas_pedras
         super().__init__(TESOURO, esquerda=acampamento, direita=ProximaCamara())
         self.pedras = PedrasPreciosas(quantas_pedras=self.tesouro)
