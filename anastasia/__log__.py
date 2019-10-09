75f8d26be28b015ddcdac1d176118ae5099ba443
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 11:58:26 -0300

    Saved anastasia/__log__.py @2019-10-09 14:58

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -107,3 +107,29 @@ AttributeError: 'PedrasPreciosas' object has no attribute 'nome'
     self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
 AttributeError: 'PedrasPreciosas' object has no attribute 'nome'
 '''},
+{'date': 'Wed Oct 09 2019 11:58:26.884 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    jogo = JogoTesouroInca()
+  module <module> line 106
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 79
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 71
+    self.pedras = PedrasPreciosas(quantas_pedras=self.tesouro)
+  module <module> line 44
+    self.pedras_especificas = [Elemento(
+  module elemento.main line 40
+    self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
+AttributeError: 'PedrasPreciosas' object has no attribute 'nome'
+'''},
