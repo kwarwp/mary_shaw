7f7fda9dc99c014aee0f56e820d4e74fcecd48d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 11:45:35 -0300

    Saved rachel/__log__.py @2019-10-09 14:45

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -324,3 +324,25 @@ ImportError: cannot import name 'Cena'
     from _spy.vitollino.main import Cena, INVENTARIO
 ImportError: cannot import name 'Cena'
 '''},
+{'date': 'Wed Oct 09 2019 11:45:35.337 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 219
+    jogo = JogoTesouroInca()
+  module <module> line 208
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 181
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 162
+    self.pedras = PredasPreciosas(quantas_pedras=self.tesouro)
+NameError: name 'PredasPreciosas' is not defined
+'''},
