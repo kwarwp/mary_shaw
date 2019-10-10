55bdaaf30cb224227f363e6438c190467d09dcc0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 17:04:11 -0300

    Saved danae/main.py @2019-10-10 20:04

--- danae/main.py
+++ danae/main.py
@@ -1,5 +1,8 @@
 # mary_shaw.danae.main.py
 from browser import document, html
+pagina = html.HEAD()
+title = html.TITLE("Olá mundo HTML")
+pagina <= title
 
 document.html = ""
-# document <= pagina
+document <= pagina
