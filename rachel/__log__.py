0ce6f5a1372720ab4e5ad4f9c6fb8d0261031612
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-12 11:40:28 -0300

    Saved rachel/__log__.py @2019-10-12 14:40

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1085,3 +1085,25 @@ AttributeError: 'Jogador' object has no attribute 'get_pedras'
     self.tesouro = self.pedras.get_pedras()
 AttributeError: 'Tesouros' object has no attribute 'pedras'
 '''},
+{'date': 'Sat Oct 12 2019 11:40:27.67 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 202
+    jogo = JogoTesouroInca()
+  module <module> line 191
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 164
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 151
+    self.tesouro = pedras.get_pedras()
+AttributeError: 'Jogador' object has no attribute 'get_pedras'
+'''},
