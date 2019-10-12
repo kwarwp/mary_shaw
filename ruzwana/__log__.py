ef624c2ccc12452b1058f28353e80328daca91c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-12 12:13:33 -0300

    Saved ruzwana/__log__.py @2019-10-12 15:13

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -25,3 +25,19 @@
     self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
 AttributeError: 'PedrasPreciosas' object has no attribute 'nome'
 '''},
+{'date': 'Sat Oct 12 2019 12:13:32.922 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    INVENTARIO.help()
+AttributeError: 'Inventario' object has no attribute 'help'
+'''},
