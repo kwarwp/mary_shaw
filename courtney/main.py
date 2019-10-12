b20b87238b8052a53a458b5fbb558cb9dd6af6d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-12 10:14:31 -0300

    Saved courtney/main.py @2019-10-12 13:14

--- courtney/main.py
+++ courtney/main.py
@@ -3,7 +3,7 @@
 """
     Tesouro Inca
 """
-from _spy.vitollino.main import Cena, INVENTARIO
+from _spy.vitollino.main import Cena, INVENTARIO, Texto
 from elemento.main import Elemento
 from random import choice
 
@@ -21,7 +21,9 @@ TUMBA = [COBRA, MUMIA] + [TESOURO]*3
 class Jogador():
     """ Um explorador em busca de tesouros """
     def __init__(self):
-        """ Inicia com tesouro """
+        """ Inicia com tesouro , com acampamento
+            :param acampamento: o acampamento do jogador
+        """
         self.tesouro = 0
     
     def ganho(self, valor):
@@ -31,7 +33,33 @@ class Jogador():
         self.tesouro += valor
         [INVENTARIO.bota("turquesa", TURQUESA) for _ in range(valor)]
 
+    def desiste(self):
+        """ Remove as pedras do inventario e coloca na cabana """
 
+
+class PedrasPreciosas:
+    """ Pedras que integram o tesouro"""
+    def __init__(self, quantas_pedras=4):
+        """ Inicia aa coleção de padras com uma quantidade
+            :param int quantas_pedras: numero de pedras neste tesouro
+        """
+        # = MODELO Conceitual = 
+        self.tesouro_contabil = quantas_pedras
+        # = VISTA = Modelo visual de como apresentar o tesouro
+        self.limbo_onde_as_pedras_desaparecem = Cena()
+        self.pedras_especificas = [Elemento(
+             TURQUESA, x=50+50*pedra, y=250, w=40, h=40) for pedra in
+             range(self.tesouro_contabil)]
+             
+    def representa(self, local):
+        """ Apresenta as pedras organizadas em um local """
+        for pedra in self.pedras_especificas:
+            pedra.entra(local)
+             
+    def some(self):
+        """ Desaparece com as pedras organizadas em um local """
+        for pedra in self.pedras_especificas:
+            pedra.entra(self.limbo_onde_as_pedras_desaparecem)
 
 class Tesouros(Cena):
     """ Camaras secretas contendo tesouros """
@@ -41,15 +69,23 @@ class Tesouros(Cena):
         """
         class ProximaCamara:
             def vai(self):
-                pedras_na_camara = choice(range(1,5))
-                Tesouros(pedras_na_camara, acampamento, eu).vai()
-                eu.ganho(pedras_na_camara)
+                self.pedras = pedras_na_camara = choice(range(1,5))
+                self.c = camara = Tesouros(pedras_na_camara, acampamento, eu)
+                camara.vai()
+                Texto(camara, 
+                      "Você encontra um tesouro!",
+                      foi=self.pega_tesouro).vai()
+            def pega_tesouro(self):
+                eu.ganho(self.pedras)
+                self.pedras = 0
+                self.c.esvazia_camara()
         self.tesouro = quantas_pedras
         super().__init__(TESOURO, esquerda=acampamento, direita=ProximaCamara())
-        #self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
-        self.pedras = [Elemento(
-             TURQUESA, x=50+50*pedra, y=250, w=40, h=40, cena=self) for pedra in
-             range(self.tesouro)]
+        self.pedras = PedrasPreciosas(quantas_pedras=self.tesouro)
+        self.pedras.representa(self)
+    def esvazia_camara(self):
+        self.tesouro = 0
+        self.pedras.some()
 
 
 class Tumba():
