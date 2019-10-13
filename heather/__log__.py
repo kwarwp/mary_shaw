de2105ad1d35287f0f2ec239a5a65de8dbc5226b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-13 11:38:18 -0300

    Created heather/__log__.py @2019-10-13 14:38

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Oct 13 2019 11:38:16.820 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from vitollino.main import Cena Elemento, STYLE
+                                          ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
