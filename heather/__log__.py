58a4cd5a6948d06f75038d590cccd0aff2e9f056
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-13 11:54:43 -0300

    Saved heather/__log__.py @2019-10-13 14:54

--- heather/__log__.py
+++ heather/__log__.py
@@ -19,3 +19,9 @@ Exception: obj is undefined
   module '$exec_1835' line 9
 [Elemento(LOREM.format(theme), x=10+(count*200)%1400, y=10+(count*200)//1400, w=190, h=190, cena=cena) 
 '''},
+{'date': 'Sun Oct 13 2019 11:54:42.918 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: undefined has no properties
+  module '$exec_1843' line 9
+[Elemento(LOREM.format(theme), x=10+(count*200)%1400, y=10+(count*200)//1400, w=190, h=190, cena=cena) for count, theme in enumerate(TH)]
+'''},
