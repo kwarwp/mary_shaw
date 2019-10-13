515cb5c415b3a5d02a80d787832ebc91dba03443
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-13 11:48:02 -0300

    Saved heather/__log__.py @2019-10-13 14:48

--- heather/__log__.py
+++ heather/__log__.py
@@ -6,3 +6,10 @@
                                           ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Sun Oct 13 2019 11:48:01.491 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  [Elemento(LOREM.format(choice(THEMES)), x=10+(count*200)%1400, y=10+(count*200)//1400 w=190, h=190, cena=cena) 
+                                                                                         ^
+SyntaxError: invalid syntax
+'''},
