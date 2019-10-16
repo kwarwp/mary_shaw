87d56fc202d3dc1ff63178a127352e6f69037b6b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:27:26 -0300

    Saved kathryn/__log__.py @2019-10-16 15:27

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -142,3 +142,22 @@ Traceback (most recent call last):
     self.camara.entra()
 AttributeError: 'int' object has no attribute 'entra'
 '''},
+{'date': 'Wed Oct 16 2019 12:26:05.627 GMt-0300 (hora estándar de Brasilia) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros do Templo Inca
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    TemploInca().inicia()
+  module <module> line 59
+    camara.entra()
+NameError: name 'camara' is not defined
+'''},
