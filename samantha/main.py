08a6c14a965386bd0bbf8e201904ac864470fa87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:42:58 -0300

    Saved samantha/main.py @2019-10-16 15:42

--- samantha/main.py
+++ samantha/main.py
@@ -1,9 +1,11 @@
 # mary_shaw.samantha.main.py
+from random import randint
 """
 Uma expedição para coletar os tesouros do Templo Inca
  --Relato:
  fui e voltei rico
 """
+
 __author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
 
 
@@ -12,14 +14,18 @@ class Explorador:
     def __init__(self):  # (self, camara)
         self.mochila = 0
         self.cabana = 0
-        # self.camara = camara
+        # self.camara = camara?
             
     def pega(self, quantidade, camara):
         """ coloca um tesouro na mochila """
         print(f"Você coloca {quantidade} tesouro na mochila ")
         self.mochila += quantidade
         print(f"Você fica com {self.mochila} tesouros na mochila ")
-        camara.entra(self)
+        if input("Continua?").lower() == "s":
+            camara.entra(self)
+        else:
+            self.sai()
+        
             
     def sai(self):
         """ sai do templo """
