f601a0ce26bdc84786e37b48bf3328d228bd68bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:16:47 -0300

    Saved adda/__log__.py @2019-10-16 15:16

--- adda/__log__.py
+++ adda/__log__.py
@@ -203,3 +203,22 @@ Traceback (most recent call last):
     Camara.entra(self.explorador)
 TypeError: entra() missing 1 positional argument: explorador
 '''},
+{'date': 'Wed Oct 16 2019 12:16:46.207 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros de um templo Inca
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    TemploInca().inicia()
+  module <module> line 50
+    Camara.entra(Explorador())
+TypeError: entra() missing 1 positional argument: explorador
+'''},
