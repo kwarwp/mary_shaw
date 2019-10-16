8cfb8c8a7e7ed907e2ec3606b94f5029002b3b58
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 11:53:01 -0300

    Saved parisa/__log__.py @2019-10-16 14:53

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -111,3 +111,30 @@ Traceback (most recent call last):
     (TemploInca.mochila)
 AttributeError: 'TemploInca' object has no attribute 'mochila'
 '''},
+{'date': 'Wed Oct 16 2019 11:53:01.493 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma aventura para coletar os tesouros do Templo Inca
+Você encontrou tesouro!
+Você coloca 1 tesouro na mochila.
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    TemploInca().inicia()
+  module <module> line 45
+    self.entra()
+  module <module> line 52
+    self.explorador.pega(1, self)
+  module <module> line 23
+    print (f"Você fica com {TemploInca.mochila} tesouros na mochila.")
+  module <module> line 1
+    (TemploInca.mochila)
+AttributeError: 'TemploInca' object has no attribute 'mochila'
+'''},
