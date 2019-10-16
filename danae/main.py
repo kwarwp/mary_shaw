6ee5979d7e56c7b1dc371986077ace81ac95ca66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:19:20 -0300

    Saved danae/main.py @2019-10-16 13:19

--- danae/main.py
+++ danae/main.py
@@ -1,1 +1,12 @@
 # mary_shaw.danae.main.py
+"""
+Uma expedição para coletar os tesouros do Templo Inca
+ --Relato:
+ fui e voltei rico
+"""
+__author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
+
+
+
+
+
