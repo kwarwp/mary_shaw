dbaa303b4940b45c05cadc3d9741a461cd058056
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 22:07:03 -0300

    Saved courtney/__log__.py @2019-10-16 01:07

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -43,3 +43,27 @@ AttributeError: 'Tesouros' object has no attribute 'pedrasx'
     self.pedras = equivalencia()
 NameError: name 'equivalencia' is not defined
 '''},
+{'date': 'Tue Oct 15 2019 22:07:02.341 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 158
+    jogo = JogoTesouroInca()
+  module <module> line 147
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 120
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 85
+    self.pedras = self.equivalencia()
+  module <module> line 99
+    soma = self.pedras.sum() 
+AttributeError: 'PedrasPreciosas' object has no attribute 'sum'
+'''},
