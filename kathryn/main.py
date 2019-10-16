9b456e37ec991b914d95c78e5bb8383643eb09d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 11:11:40 -0300

    Saved kathryn/main.py @2019-10-16 14:11

--- kathryn/main.py
+++ kathryn/main.py
@@ -11,14 +11,29 @@ __author__ = "Pablo LV"
 class TemploInca:
     cabana = 0
     mochila = 0
-    camara = 3
+    TOTAL_CAMARAS = 3
+    camara_atual = 0
     #-----------------------------------------------------------------------
     def inicia(self):
         print("Uma Expedicao para COletar os Tesouros do Templo Inca")
         self.entra()
     #-----------------------------------------------------------------------
     def entra(self):
-        print("Vocé entra em uma camara do Templo")
+        TemploInca.camara_atual = TemploInca.camara_atual + 1
+        print("ENTRANDO PARA CAMARA ", TemploInca.camara_atual)
+        print("-----------------------------")
+        print(f"Vocé entra em uma camara {TemploInca.camara_atual} do Templo")
+        #if TemploInca.TOTAL_CAMARAS == 0:
+        if TemploInca.camara_atual == TemploInca.TOTAL_CAMARAS:
+            print("Stop")
+        else:
+            self.pega(1)
+    #-----------------------------------------------------------------------
+    def pega(self, quantidade):
+        print(f"Voce coloca {quantidade} tesouro na mochila")
+        TemploInca.mochila = TemploInca.mochila + quantidade
+        print(f"Voce fica com {TemploInca.mochila} tesouros na mochila")
+        self.entra()
     #-----------------------------------------------------------------------
 #===========================================================================
 #===========================================================================
