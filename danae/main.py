82b0b3292bd5deda91c7b80692509f00d02830bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:24:29 -0300

    Saved danae/main.py @2019-10-16 13:24

--- danae/main.py
+++ danae/main.py
@@ -7,6 +7,16 @@ Uma expedição para coletar os tesouros do Templo Inca
 __author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
 
 
+class TemploInca:
+    cabana = 0
+    mochila = 0
+    camara = 3
+    def inicia(self):
+        """ inicia a exploração """
+        print("Uma expedição para coletar os tesouros do Templo Inca")
 
+
+if __name__ == "__main__":
+    Templo().inicia()
 
 
