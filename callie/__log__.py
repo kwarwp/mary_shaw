e2052aa181f8dbdb975d89554b6a147c123ab55c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:31:05 -0300

    Saved callie/__log__.py @2019-10-16 13:31

--- callie/__log__.py
+++ callie/__log__.py
@@ -13,3 +13,10 @@ IndentationError: unexpected indent
                                                                                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 16 2019 10:30:57.782 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  def inicial(self:
+                                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
