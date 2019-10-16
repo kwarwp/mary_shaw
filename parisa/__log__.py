ce318127baa78fdb586972efc7c7b99e805a31b8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:15:12 -0300

    Saved parisa/__log__.py @2019-10-16 15:15

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -165,3 +165,22 @@ Traceback (most recent call last):
     (TemploInca.mochila)
 AttributeError: 'TemploInca' object has no attribute 'mochila'
 '''},
+{'date': 'Wed Oct 16 2019 12:15:12.390 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma aventura para coletar os tesouros do Templo Inca
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    TemploInca().inicia()
+  module <module> line 64
+    self.entra()
+AttributeError: 'TemploInca' object has no attribute 'entra'
+'''},
