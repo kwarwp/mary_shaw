ad62ac2803174e7518e4b862caf762b7cdc625e8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:36:55 -0300

    Saved morgan/main.py @2019-10-16 15:36

--- morgan/main.py
+++ morgan/main.py
@@ -9,40 +9,68 @@ Nossa aventura em busca do tesouro inca consiste no descobrimento do tesouro inc
 
 __author__ = "Rafael Ris-Ala (rafaelrisala@ufrj.br)"
 
+from random import randint
 
 
-class TemploInca:
-    cabana = 0
-    mochila = 0
-    camara = 3
-    cabana = 0
-    
-    def inicia(self):
-        """ inicia a exploração """
-        print("Uma expedição para coletar os tesouros do Templo Inca")
-        self.entra()
+class Explorador:
+    """ explora o templo inca"""
+    def __init__(self):  # (self, camara)
+        self.mochila = 0
+        self.cabana = 0
+        # self.camara = camara
+            
+    def pega(self, quantidade, camara):
+        """ coloca um tesouro na mochila """
+        print(f"Você coloca {quantidade} tesouro na mochila ")
+        self.mochila += quantidade
+        print(f"Você fica com {self.mochila} tesouros na mochila ")
+        camara.entra()
+            
+    def sai(self):
+        """ sai do templo """
+        print("Você sai do templo e guarda os tesouros!")
+        self.cabana, self.mochila = self.mochila, 0
+        print(f"Você ficou com {self.cabana} tesouros na cabana!")
+
+
+class Camara:
+    def __init__(self, explorador):
+        self.camara = 3
+        self.explorador = explorador
+        #self.quantos_tesouros = tesouros
         
     def entra(self):
         """ entra em uma câmara"""
         print("Você entra em uma câmara com tesouros!")
-        TemploInca.camara -= 1        
-        if TemploInca.camara:
-            self.pega(1)
+        if self.camara:
+            self.camara -= 1        
+            self.explorador.pega(randint(1, 4), self)
         else:
-            self.sair()
-            
-    def pega(self, quantidade):
-        """ coloca um tesouro na mochila """
-        print(f"Você coloca {quantidade} tesouro na mochila ")
-        TemploInca.mochila += quantidade
-        print(f"Você fica com {TemploInca.mochila} tesouros na mochila ")
-        self.entra()
+            print("Não havia mais tesouros!")
+            self.explorador.sai()
+
+
+class TemploInca:
+    camara = 3
+    def __init__(self):
+        # self.camara = Camara()
+        self.explorador = Explorador()  # (self) (self.camara)
+        self.camara = Camara(self.explorador)
+
+    def inicia(self):
+        """ inicia a exploração """
+        print("Uma expedição para coletar os tesouros do Templo Inca")
+        self.camara.entra()
         
-    def sair(self):
-        """ sair do jogo e acumular na cabana com o que há na mochila """
-        print("sair do jogo e acumular na cabana com o que há na mochila")
-        self.cabana += self.mochila
-        print("Cabana: ",self.cabana)
+    def __entra(self):
+        """ entra em uma câmara"""
+        print("Você entra em uma câmara com tesouros!")
+        if TemploInca.camara:
+            TemploInca.camara -= 1        
+            self.explorador.pega(1, self)
+        else:
+            print("Não havia mais tesouros!")
+            self.explorador.sai()
 
 
 if __name__ == "__main__":
