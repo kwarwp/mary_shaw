39bd9c37162b9d4ca40ca8fcfc5a1a5c39d74543
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 11:56:57 -0300

    Saved anastasia/__log__.py @2019-10-16 14:56

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -241,3 +241,19 @@ TypeError: entra() takes 1 positional argument but more were given
                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 16 2019 11:56:55.967 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    TemploInca().inicia()
+AttributeError: 'TemploInca' object has no attribute 'inicia'
+'''},
