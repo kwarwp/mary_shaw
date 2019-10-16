bdd086ad77361c31769ef3b30796b9453a56705b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 17:49:15 -0300

    Saved courtney/__log__.py @2019-10-16 20:49

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -206,3 +206,10 @@ AttributeError: 'GUI' object has no attribute 'mochila'
     (self.mochila)
 AttributeError: 'GUI' object has no attribute 'mochila'
 '''},
+{'date': 'Wed Oct 16 2019 17:49:14.610 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  """ Exibir o tesouro capturado """    
+  ^
+IndentationError: expected an indented block
+'''},
