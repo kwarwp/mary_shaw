b1e182dbc3233654839b532ffda89c8164c70d43
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:45:12 -0300

    Saved adda/main.py @2019-10-16 13:45

--- adda/main.py
+++ adda/main.py
@@ -24,6 +24,8 @@ class TemploInca:
     def pega(self, quantidade):
         """ guarda um tesouro na machila """
         print(f"Você coloca {quantidade} tesouro na mochila" )
+        TemploInca.mochila += quantidade
+        print(f"Você fica com {TemploInca.mochila} tesouros na mochila")
         
 if __name__ == "__main__":
     TemploInca().inicia()
