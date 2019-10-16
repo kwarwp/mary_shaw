8fd151b6a4b0a32ad704347085376b2298cb0313
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 11:16:07 -0300

    Saved adda/__log__.py @2019-10-16 14:16

--- adda/__log__.py
+++ adda/__log__.py
@@ -67,3 +67,48 @@ TypeError: __init__() takes 2 positional argument but more were given
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Oct 16 2019 11:16:03.542 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros de um templo Inca
+O que existe nela
+Você coloca 1 tesouro na mochila
+Você fica com 1 tesouros na mochila
+O que existe nela
+Você coloca 1 tesouro na mochila
+Você fica com 2 tesouros na mochila
+O que existe nela
+Você coloca 1 tesouro na mochila
+Você fica com 3 tesouros na mochila
+O que existe nela
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    TemploInca().inicia()
+  module <module> line 17
+    self.entra()
+  module <module> line 24
+    self.pega(1)
+  module <module> line 39
+    self.entra()
+  module <module> line 24
+    self.pega(1)
+  module <module> line 39
+    self.entra()
+  module <module> line 24
+    self.pega(1)
+  module <module> line 39
+    self.entra()
+  module <module> line 26
+    print(f"A rodada acabou para você, leve suas {Templo.cabana} para cabana")
+  module <module> line 1
+    (Templo.cabana)
+NameError: name 'Templo' is not defined
+'''},
