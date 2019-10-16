74fceb83ff7a1da1d99954d84114ee25e560d173
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:29:58 -0300

    Created callie/__log__.py @2019-10-16 13:29

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 16 2019 10:29:24.292 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  mochila = 0
+  ^
+IndentationError: unexpected indent
+'''},
