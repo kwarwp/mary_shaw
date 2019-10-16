e75f303e027adc8c1ebffb2ce1f6e8c5f1e5041c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:16:10 -0300

    Saved lisa/__log__.py @2019-10-16 15:16

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -145,3 +145,10 @@ SyntaxError: invalid syntax
                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 16 2019 12:16:11.202 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  def sai(self)
+                ^
+SyntaxError: invalid syntax
+'''},
