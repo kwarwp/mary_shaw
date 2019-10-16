38aed09934e47b232d962dffa0820ae1c0ede53f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:47:59 -0300

    Saved morgan/__log__.py @2019-10-16 13:47

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -68,3 +68,28 @@ SyntaxError: EOL while scanning string literal
                                              ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Oct 16 2019 10:47:59.540 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros do Templo Inca
+Você entra em uma câmara com tesouros!
+Você coloca 1 tesouro na mochila
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    TemploInca().inicia()
+  module <module> line 20
+    self.entra()
+  module <module> line 25
+    self.pega(1)
+  module <module> line 31
+    print("Você coloca %s tesouro na mochila" (1))
+TypeError: 'str' object is not callable
+'''},
