ab618a59e2f6b414265dca703b15550741ec6ddf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:28:26 -0300

    Saved samantha/__log__.py @2019-10-16 15:28

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -149,3 +149,29 @@ TypeError: __init__() missing 1 positional argument: explorador
     self.camara = Camara()
 TypeError: __init__() missing 1 positional argument: explorador
 '''},
+{'date': 'Wed Oct 16 2019 12:28:26.764 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros do Templo Inca
+Você entra em uma câmara com tesouros!
+Você coloca 1 tesouro na mochila 
+Você fica com 1 tesouros na mochila 
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    TemploInca().inicia()
+  module <module> line 55
+    self.camara.entra(self.explorador)
+  module <module> line 41
+    explorador.pega(1, self)
+  module <module> line 22
+    camara.entra()
+TypeError: entra() missing 1 positional argument: explorador
+'''},
