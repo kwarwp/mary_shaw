62b48ab0aec592b42a794c889ce40d61cc56a9f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:45:01 -0300

    Saved lisa/main.py @2019-10-16 13:45

--- lisa/main.py
+++ lisa/main.py
@@ -22,3 +22,13 @@ class TemploInca:
     def entra(self):
         """ entra em uma câmara """
         print("Você entra em uma câmara com tesouros!")
+        self.pega(1)
+        
+    def pega(self,quantidade):
+        """ coloca um tesouro na mochila """
+        print(f"Você coloca {quantidade} tesouro na mochila ")
+        TemploInca.mochila + = quantidade
+        print(f"Você fica {TemploInca.mochila} tesouro na mochila ")
+        
+if __name__ == "__main__":
+    TemploInca().inicia()
