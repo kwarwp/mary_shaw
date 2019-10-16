30eaa671f7cce91551c08031ac820d746b81eea2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 13:05:00 -0300

    Saved lisa/main.py @2019-10-16 16:04

--- lisa/main.py
+++ lisa/main.py
@@ -11,51 +11,69 @@ Continuei e ao sair o segundo perigo todos nós deixamos para trás o que tinham
 """
 
 class Explorador:
-    """ explora o templo inca """
-    def __init__(self):
+    """ explora o templo inca"""
+    def __init__(self):  # (self, camara)
         self.mochila = 0
-        
-    def pega(self,quantidade):
+        self.cabana = 0
+        # self.camara = camara
+            
+    def pega(self, quantidade, camara):
         """ coloca um tesouro na mochila """
         print(f"Você coloca {quantidade} tesouro na mochila ")
         self.mochila += quantidade
-        print(f"Você fica {TemploInca.mochila} tesouro(s) na mochila ")
-        
+        print(f"Você fica com {self.mochila} tesouros na mochila ")
+        camara.entra()
+            
     def sai(self):
         """ sai do templo """
         print("Você sai do templo e guarda os tesouros!")
-        self.cabana, self.mochila = self.mochila, 0
+        self.cabana += self.mochila
+        self.mochila - 0
         print(f"Você ficou com {self.cabana} tesouros na cabana!")
-        
+        if input("Nova Incursão?")
+#class Cabana:
+    #def __init__(self):
+        #self.
+
 class Camara:
-    def __init__(self)
-       self.camara = 3
-        self.tesouro =
+    def __init__(self):
+        self.explorador = Explorador()
+        self.camaraquantidade = 3
 
     def entra(self):
         """ entra em uma câmara """
         print("Você entra em uma câmara com tesouros!")
         self.pega(1)
-        self.camara -= 1
-        if self.camara:
+        self.camaraquantidade -= 1
+        if self.camaraquantidade:
             self.pega(1)
         else:
             print("Não havia mais tesouros!")
             self.sai()
 
 
-class TemploInca:    
+class TemploInca:
+    camara = 3
     def __init__(self):
-    #self.camara = Camara()
-    self.explorador = Explorador() #(self) (self.camara)
-    
+        # self.camara = Camara()
+        self.explorador = Explorador()  # (self) (self.camara)
+
     def inicia(self):
         """ inicia a exploração """
         print("Uma expedição para coletar os tesouros do Templo Inca")
         self.entra()
         
-   
         
+    def entra(self):
+        """ entra em uma câmara"""
+        print("Você entra em uma câmara com tesouros!")
+        if TemploInca.camara:
+            TemploInca.camara -= 1        
+            self.explorador.pega(1, self)
+        else:
+            print("Não havia mais tesouros!")
+            self.explorador.sai()
+
 
 if __name__ == "__main__":
     TemploInca().inicia()
