425a6c8970d3d4aeec04cc0abb5c4297b7703397
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:27:54 -0300

    Saved courtney/__log__.py @2019-10-16 15:27

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -166,3 +166,23 @@ AttributeError: 'Tesouros' object has no attribute 'representa'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Oct 16 2019 12:27:53.283 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    class TemploInca:
+  module <module> line 23
+    print(f"Você fica com {self.mochila} tesouros na mochila ")
+  module <module> line 1
+    (self.mochila)
+AttributeError: 'GUI' object has no attribute 'mochila'
+'''},
