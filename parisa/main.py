5531903d2127a05ec11aeb4065b901a6aa56539c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 11:37:44 -0300

    Saved parisa/main.py @2019-10-16 14:37

--- parisa/main.py
+++ parisa/main.py
@@ -11,46 +11,50 @@
 __author__ = "Fernanda Prazeres <fprazeres at dcc ufrj br>"
 
 class Explorador:
-    mochila = 0
-    cabana = 0
-    def 
+    """ explora o templo inca """
+    def __init__(self):
+        self.mochila = 0
+        self.cabana = 0
+                
+        def pega(self, quantidade, camara):
+        """ Coloca um tesouro na mochila """
+        print (f"Você coloca {quantidade} tesouro na mochila.")
+        self.mochila += quantidade 
+        print (f"Você fica com {TemploInca.mochila} tesouros na mochila.")
+        camara.entra()
+        
+        def sai(self):
+        """ Sai do Templo """
+        print (f"Você saiu do Templo e voltou para sua Cabana")
+        self.cabana = self.mochila
+        self.mochila = 0
+        print (f"Você ficou com {TemploInca.cabana} tesouros na sua Cabana.")
+    
 
 
 class TemploInca:
     cabana = 0
-    mochila = 0
     camara = 3 
+    
+    def __init__(self):
+        self.explorador = Explorador()
+        self.cabana = Explorador()
     
     def inicia (self):
         """ inicia a aventura """
         print ("Uma aventura para coletar os tesouros do Templo Inca")
         self.entra()
-        
+                  
     def entra (self):
         """ entra na câmara """
         print ("Você encontrou tesouro!")
         if TemploInca.camara:
             TemploInca.camara -= 1
-            self.pega(1)
+            self.explorador.pega(1)
         else:
             print ("Não há mais tesouros!")
-            self.sai()    
-        
-    def pega(self, quantidade):
-        """ Coloca um tesouro na mochila """
-        print (f"Você coloca {quantidade} tesouro na mochila.")
-        TemploInca.mochila += quantidade 
-        print (f"Você fica com {TemploInca.mochila} tesouros na mochila.")
-        self.entra()
-        
-    def sai(self):
-        """ Sai do Templo """
-        print (f"Você saiu do Templo e voltou para sua Cabana")
-        TemploInca.cabana = TemploInca.mochila
-        TemploInca.mochila = 0
-        print (f"Você ficou com {TemploInca.cabana} tesouros na sua Cabana.")
-    
-        
+            self.explorador.sai()    
+            
 if __name__ == "__main__":
       TemploInca().inicia()
         
