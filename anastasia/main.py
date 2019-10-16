05f8fdc4af7f6bb01a579ba9c6d2e5ced3f7056d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:31:32 -0300

    Saved anastasia/main.py @2019-10-16 13:31

--- anastasia/main.py
+++ anastasia/main.py
@@ -17,7 +17,7 @@ class TemploInca:
     camara = 3
     def inicia(self):
         """ inicia a exploração """
-        print("Uma expedição para coletar os Tesouro Inca")
+        print("Uma expedição para coletar os tesouros Inca")
         self.entra()
         
     def entra(self):
