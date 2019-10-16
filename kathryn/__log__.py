59023832927f14f2623d4da1f9512cb2e5571701
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:26:07 -0300

    Saved kathryn/__log__.py @2019-10-16 13:26

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -57,3 +57,19 @@ SyntaxError: invalid syntax
     print(JogoTesouroInca.cena_do_templo(),JogoTesouroInca(),JogoTesouroInca())
 AttributeError: 'JogoTesouroInca' object has no attribute 'cena_do_templo'
 '''},
+{'date': 'Wed Oct 16 2019 10:24:45.139 GMt-0300 (hora estándar de Brasilia) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    jogo = JogoTesouroInca()
+NameError: name 'JogoTesouroInca' is not defined
+'''},
