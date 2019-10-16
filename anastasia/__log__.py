0413d18b85bbad96fe16e02b03996b4c9f730f98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:15:44 -0300

    Saved anastasia/__log__.py @2019-10-16 15:15

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -279,3 +279,25 @@ Traceback (most recent call last):
     self.explorador.pega(1, self) 
 AttributeError: 'TemploInca' object has no attribute 'explorador'
 '''},
+{'date': 'Wed Oct 16 2019 12:15:43.748 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros do Templo Inca
+Você entra em uma câmara com tesouros!
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 63
+    TemploInca().inicia()
+  module <module> line 49
+    self.entra()
+  module <module> line 56
+    self.explorador.pega(1, self) 
+AttributeError: 'TemploInca' object has no attribute 'explorador'
+'''},
