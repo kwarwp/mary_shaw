d4164a184dd4bfe333fe24636f0d76cd4b44002e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:15:49 -0300

    Saved adda/__log__.py @2019-10-16 15:15

--- adda/__log__.py
+++ adda/__log__.py
@@ -184,3 +184,22 @@ Traceback (most recent call last):
     self.Camara.entra(self.explorador)
 AttributeError: 'TemploInca' object has no attribute 'Camara'
 '''},
+{'date': 'Wed Oct 16 2019 12:15:48.145 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros de um templo Inca
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    TemploInca().inicia()
+  module <module> line 50
+    Camara.entra(self.explorador)
+TypeError: entra() missing 1 positional argument: explorador
+'''},
