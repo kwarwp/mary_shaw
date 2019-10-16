e428b54369121b4e0bbe248d10ec24c32ef6dcf4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:30:48 -0300

    Saved anastasia/__log__.py @2019-10-16 13:30

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -185,3 +185,19 @@ AttributeError: 'PedrasPreciosas' object has no attribute 'nome'
     self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
 AttributeError: 'PedrasPreciosas' object has no attribute 'nome'
 '''},
+{'date': 'Wed Oct 16 2019 10:30:52.970 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    if __name == "__main__":
+NameError: name '__name' is not defined
+'''},
