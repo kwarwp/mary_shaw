303b2e329699e063bab6e912e77f01b84edf49e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 13:06:04 -0300

    Saved parisa/__log__.py @2019-10-16 16:06

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -392,3 +392,29 @@ TypeError: __init__() takes 1 positional argument but more were given
     self.explorador = Explorador()
 TypeError: __init__() missing 1 positional argument: templo_inca
 '''},
+{'date': 'Wed Oct 16 2019 13:06:04.287 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 89
+    TemploInca().inicia()
+  module <module> line 80
+    self.camara = Camara(self.explorador)
+TypeError: __init__() takes 1 positional argument but more were given
+'''},
