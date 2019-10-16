7dc216176d6b7d41e7a735bb04eb017985c1f671
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:18:03 -0300

    Saved lisa/__log__.py @2019-10-16 15:18

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -152,3 +152,10 @@ SyntaxError: invalid syntax
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 16 2019 12:18:05.278 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  Tesouro = 1
+  ^
+IndentationError: expected an indented block
+'''},
