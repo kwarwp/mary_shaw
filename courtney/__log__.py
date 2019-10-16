913f51e8a48502595350c47ce87ae73787559302
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 22:07:51 -0300

    Saved courtney/__log__.py @2019-10-16 01:07

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -91,3 +91,25 @@ AttributeError: 'PedrasPreciosas' object has no attribute 'sum'
     soma = self.pedras.sum() 
 AttributeError: 'PedrasPreciosas' object has no attribute 'sum'
 '''},
+{'date': 'Tue Oct 15 2019 22:07:50.392 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 158
+    jogo = JogoTesouroInca()
+  module <module> line 147
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 120
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 85
+    self.pedras = self.equivalencia(self)
+TypeError: equivalencia() takes 1 positional argument but more were given
+'''},
