1bb7a54cb5ecccec4d48068a7665d176c15da66a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:47:46 -0300

    Saved samantha/main.py @2019-10-16 13:47

--- samantha/main.py
+++ samantha/main.py
@@ -1,63 +1,34 @@
 # mary_shaw.samantha.main.py
-
-# from adda.main import JogoTesouroInca as JogoMarilia
 """
-    Tesouro Inca
+Uma expedição para coletar os tesouros do Templo Inca
+ --Relato:
+ fui e voltei rico
 """
-from _spy.vitollino.main import Cena, INVENTARIO, Elemento
-from elemento.main import Elemento
-from random import choice
-
-__author__ = "Carlo"
-IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
-MUMIA = "https://i.imgur.com/YGV3CPB.jpg"
-COBRA = "https://i.imgur.com/IeC0gBj.jpg"
-TESOURO = "https://i.imgur.com/xkBdHCE.jpg"
-TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
-ACAMPAMENTO = "https://i.imgur.com/dmSDeDF.jpg"
-
-TUMBA = [COBRA, MUMIA] + [TESOURO]*3
+__author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
 
 
-class Jogador():
-    """ Um explorador em busca de tesouros """
-    def __init__(self):
-        """ Inicia com tesouro """
-
-
-class Tumba():
-    """ Um complexo de camaras secretas sob o templo """
-    def __init__(self):
-        """ Inicia o complexo de camaras """
-
-
-class Acampamento(Cena):
-    """ Um lugar seguro e quentinho para admirar seus ganhos """
-    def __init__(self, cena):
-        """ Cria a cena de um acampamento com tesouros """
-        self.tesouro = 0
-        super().__init__(cena)
-        self.pedra = Elemento(TURQUESA, cena=self)
-        
-    def ganho(self, valor):
-        """ aumenta o tesouro com um valor de pedras """
-        self.tesouro += valor
-
-
-class JogoTesouroInca:
-    """ Representa o Jogo principal """
-    def __init__(self):
-        """ Constroi a cena"""
-        self.acampamento = Acampamento(ACAMPAMENTO)
-        self.cena_do_templo = Cena(IMAGEM_DO_TEMPLO, self.acampamento)
-        self.acampamento.direita = self.cena_do_templo
-        
+class TemploInca:
+    cabana = 0
+    mochila = 0
+    camara = 3
     def inicia(self):
-        """ Inicia a construção do Jogo """
-        self.cena_do_templo.vai()
+        """ inicia a exploração """
+        print("Uma expedição para coletar os tesouros do Templo Inca")
+        self.entra()
+        
+    def entra(self):
+        """ entra em uma câmara """
+        print("Você entrou em uma câmara com tesouros!")
+        self.pega(1)
+        
+    def pega(self,quantidade):
+        """ coloca um tesouro na mochila """
+        print(f"Você coloca {quantidade} tesouro na mochila")
+        TemploInca.mochila += quantidade
+        print(f"Você fica com {TemploInca.mochila} tesouros na mochila")
         
         
 if __name__ == "__main__":
-    jogo = JogoTesouroInca()
-    jogo.inicia()
-    print(JogoTesouroInca,JogoTesouroInca())
+    TemploInca().inicia()
+
+
