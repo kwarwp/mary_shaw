a19a2b78dcb449e60b999cd1e3b0f099fe36ce27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 13:02:13 -0300

    Saved adda/main.py @2019-10-16 16:02

--- adda/main.py
+++ adda/main.py
@@ -7,36 +7,61 @@ e podiamos escolher entre ficar ou sair.
 """
 __autor__ = "Marília Campos Galvão"
 
+from random import randint
+
+class Explorador:
+    def __init__(self, templo_inca):
+        self.mochila = 0
+        self.cabana = 0
+        self.templo = templo_inca
+        
+    def pega(self, quantidade, camara):
+        """ guarda um tesouro na machila """
+        print(f"Você coloca {quantidade} tesouro na mochila" )
+        self.mochila += quantidade
+        print(f"Você fica com {self.mochila} tesouros na mochila")
+        if input("Continua? (s/N)").lower() == "s":
+            camara.entra(self)
+        else:
+            self.sai()
+        
+    def sai(self):
+        """ sai do templo """
+        print("Você sai do templo e guarda os tesouros!")
+        self.cabana += self.mochila
+        self.mochila = 0
+        print(f"Você ficou com {camara.pedras} tesouros na mochila")
+        if input("Nova Incursão? (s/N)").lower() == "s":
+            self.templo.inicia()
+class Camara:
+    def __init__(self):
+        self.pedras = 3
+        
+    def entra(self, explorador):
+        """ entra em uma câmera"""
+        print("O que existe nela")
+        if self.pedras:
+            self.pedras -= 1
+            explorador.pega(randint(1, 4), self)
+        else:
+            print(f"A rodada acabou para você, leve suas {explorador.cabana} para cabana")
+            explorador.sai()
+
 class TemploInca:
-    cabana = 0
-    mochila = 0
-    camara = 3
+    def __init__(self):
+        self.explorador = Explorador(templo_inca=self)
+        self.camara = Camara(self.explorador)
+
+        
     def inicia(self):
         """ inicia a exploração """
         print("Uma expedição para coletar os tesouros de um templo Inca")
-        self.entra()
+        self.camara.entra(self.explorador)
         
-    def entra(self):
-        """ entra em uma câmera"""
-        print("O que existe nela")
-        if TemploInca.camara:
-        TemploInca.camara -= 1
-            self.pega(1)
-        else:
-            print(f"A rodada acabou para você, leve suas {Templo.cabana} para cabana")
-            self.sai()
+    
             
-    def sai(self):
-        """ sai do templo """
-        Print("Você sai do templo e guarda os tesouros!")
-        TemploInca.cabana, TemploInca.mochila = TemploInca.mochila, 0
+    
         
-    def pega(self, quantidade):
-        """ guarda um tesouro na machila """
-        print(f"Você coloca {quantidade} tesouro na mochila" )
-        TemploInca.mochila += quantidade
-        print(f"Você fica com {TemploInca.mochila} tesouros na mochila")
-        self.entra()
         
 if __name__ == "__main__":
     TemploInca().inicia()
