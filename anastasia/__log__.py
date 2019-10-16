d4c87c50a777d96df5a4087d9341e956a33b0b86
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 10:42:00 -0300

    Saved anastasia/__log__.py @2019-10-16 13:42

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -215,3 +215,22 @@ SyntaxError: invalid syntax
                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 16 2019 10:42:05.693 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros do Templo Inca
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    TemploInca().inicia()
+  module <module> line 21
+    self.entra(1)
+TypeError: entra() takes 1 positional argument but more were given
+'''},
