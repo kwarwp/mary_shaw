b284da50b8fb2c751fff074ddb10a919c70ed234
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-16 12:26:31 -0300

    Saved adda/__log__.py @2019-10-16 15:26

--- adda/__log__.py
+++ adda/__log__.py
@@ -279,3 +279,30 @@ Traceback (most recent call last):
     Camara.entra(self.explorador)
 TypeError: entra() missing 1 positional argument: explorador
 '''},
+{'date': 'Wed Oct 16 2019 12:26:30.453 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros de um templo Inca
+O que existe nela
+Você coloca 1 tesouro na mochila
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 59
+    TemploInca().inicia()
+  module <module> line 51
+    self.camara.entra(self.explorador)
+  module <module> line 37
+    explorador.pega(1, self)
+  module <module> line 19
+    print(f"Você fica com {TemploInca.mochila} tesouros na mochila")
+  module <module> line 1
+    (TemploInca.mochila)
+AttributeError: 'TemploInca' object has no attribute 'mochila'
+'''},
