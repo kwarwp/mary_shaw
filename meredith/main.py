86e1e51c45661479930c2743ed95346c0fcde3bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:37:00 -0300

    Saved meredith/main.py @2019-11-06 13:37

--- meredith/main.py
+++ meredith/main.py
@@ -4,13 +4,15 @@ Uma expedição para coletar os tesouros do Templo Inca
  --Relato:
  fui e voltei rico
  
+ 19.11.06b - usa defaultdict na Camara também
  19.11.06a - usa defaultdict como uma forma de switch
  19.11.06 - troca print por input
 """
 
-__author__ = "PJ"
-__version__ = "19.11.06a"
+__author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
+__version__ = "19.11.06b"
 from random import randint
+from collections import defaultdict
 
 
 class Explorador:
@@ -63,11 +65,25 @@ class TemploInca:
     def __init__(self):
         self.explorador = Explorador()
         self.camara = Camara()
+        self.decide = defaultdict(lambda: self.desiste)
+        self.decide["s"] = self.encara
+        '''
+        self.decide = defaultdict(lambda: input("Sábia mimimi.. macabro!"))
+        self.decide["s"] = lambda: self.camara.entra(self.explorador)
+        '''
         
     def inicia(self):
         """ inicia a exploração """
-        input("Uma expedição para coletar os tesouros do Templo Inca")
+        o_que_decidiu = input("Uma expedição para saquear o Templo Inca. Vai encarar (s/N)?")
+        self.decide[o_que_decidiu]()
+        
+    def encara(self):
+        """ decide iniciar a exploração """
         self.camara.entra(self.explorador)
+        
+    def desiste(self):
+        """ desiste da exploração """
+        input("Sábia decisão, vamos evitar este templo macabro!")
         
         
     
