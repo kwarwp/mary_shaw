fe30a1929c6edbf8aa9d53786b3caf9572e08aa3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 12:15:45 -0300

    Saved kathryn/__log__.py @2019-11-06 15:15

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -298,3 +298,23 @@ SyntaxError: invalid syntax
     camara.entra(self.explorador)
 NameError: name 'camara' is not defined
 '''},
+{'date': 'Wed Nov 06 2019 12:14:07.266 GMt-0300 (hora estándar de Brasilia) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 107
+    OBJ.inicia()
+  module <module> line 88
+    self.Seleccion[OPT.lower()]()
+  module <module> line 98
+    camara.entra(self.explorador)
+NameError: name 'camara' is not defined
+'''},
