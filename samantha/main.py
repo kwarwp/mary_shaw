deb55f400ea6a08f84bf71598fd5b5d42ed69624
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 11:34:20 -0300

    Saved samantha/main.py @2019-11-06 14:34

--- samantha/main.py
+++ samantha/main.py
@@ -20,7 +20,14 @@ class Explorador:
     def __init__(self):  # (self, camara)
         self.mochila = 0
         self.cabana = 0
+        self.perigos = "aranha cobra mumia desabamento fogo".split()
         # self.camara = camara?
+            
+    def assusta(self, tipo_perigo, camara):
+        """ se assusta com um perigo """
+        perigo = self.perigos[tipo_perigo]
+        input(f"Você se assusta com este perigo: {perigo}")
+        camara.entra(self)
             
     def pega(self, quantidade, camara):
         """ coloca um tesouro na mochila """
@@ -66,6 +73,38 @@ class Camara:
         explorador.sai()
 
 
+class CamaraPerigosa:
+    """ Uma câmara do templo.
+    o explorador usa o método entra para ter acesso aos tesouros
+    """
+    def __init__(self):
+        self.quantidade = 3
+        self.decide = defaultdict(lambda: self.desiste)
+        self.decide["s"] = self.encara
+        #self.decide[1] = self.decide[2] = self.decide[3] = self.continua
+        self.decide.update({chave: self.continua
+            for chave in range(1, self.quantidade+1)})
+        
+    def entra(self, explorador):
+        """ entra em uma câmara"""
+        o_que_decidiu = input("Você entra em uma câmara com perigos! Continua?")
+        self.decide[o_que_decidiu.lower()](explorador)
+        
+        
+    def encara(self, explorador):
+        """ decide continuar a exploração """
+        self.decide[self.quantidade](explorador)
+        
+    def continua(self, explorador):
+        """ desiste da exploração """
+        self.quantidade -= 1        
+        explorador.assusta(randint(0, 4), self)
+        
+    def desiste(self, explorador):
+        """ desiste da exploração """
+        explorador.sai()
+
+
 class TemploInca:
     """ O jogo do Tesouro Inca
     
@@ -73,7 +112,7 @@ class TemploInca:
     """
     def __init__(self):
         self.explorador = Explorador()
-        self.camara = Camara()
+        self.camara = CamaraPerigosa()
         self.decide = defaultdict(lambda: self.desiste)
         self.decide["s"] = self.encara
         
@@ -95,7 +134,7 @@ class TemploInca:
 
 
 if __name__ == "__main__":
-    #TemploInca().inicia()
-    import mary_shaw.samantha.main as mn
-    print(help(mn))
+    TemploInca().inicia()
+    #import mary_shaw.samantha.main as mn
+    #print(help(mn))
 
