f94c25965f40a1f3ba38094e6acbe8ae9e6107b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 11:47:00 -0300

    Saved samantha/__log__.py @2019-11-06 14:47

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -391,3 +391,25 @@ AttributeError: 'Camara' object has no attribute 'decide'
     self.decide[dec]()           
 AttributeError: 'Camara' object has no attribute 'decide'
 '''},
+{'date': 'Wed Nov 06 2019 12:47:00.58 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 125
+    TemploInca().inicia()
+  module <module> line 110
+    self.decide[o_que_decidiu]()
+  module <module> line 114
+    self.camara.entra(self.explorador)
+  module <module> line 88
+    self.decide[o_que_decidiu.lower()](explorador)
+AttributeError: 'CamaraPerigosa' object has no attribute 'decide'
+'''},
