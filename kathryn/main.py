815a4aab22252732842483250cb5454c6255cfac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:08:03 -0300

    Saved kathryn/main.py @2019-11-06 13:08

--- kathryn/main.py
+++ kathryn/main.py
@@ -16,17 +16,14 @@ Uma expedição para coletar os tesouros do Templo Inca
  --Relato:
  fui e voltei rico
  
+ 19.11.06a - usa defaultdict como uma forma de switch
  19.11.06 - troca print por input
 """
 
 __author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
-__version__ = "19.11.06"
+__version__ = "19.11.06a"
 from random import randint
-
-from Tkinter import *
-import tkMessageBox
-
-
+#===========================================================================
 class Explorador:
     """ explora o templo inca"""
     def __init__(self):  # (self, camara)
@@ -36,29 +33,27 @@ class Explorador:
             
     def pega(self, quantidade, camara):
         """ coloca um tesouro na mochila """
-        input(f"Você coloca {quantidade} tesouro na mochila ")
         self.mochila += quantidade
-        input(f"Você fica com {self.mochila} tesouros na mochila ")
+        input(f"Você coloca {quantidade} pedras na mochila e fica com {self.mochila} tesouros")
         camara.entra(self)
                     
     def sai(self):
         """ sai do templo """
-        input("Você sai do templo e guarda os tesouros!")
         self.cabana, self.mochila = self.mochila, 0
-        input(f"Você ficou com {self.cabana} tesouros na cabana!")
-
-
+        input(f"Você sai do templo e guarda {self.cabana} tesouros na cabana!")
+#===========================================================================
 class Camara:
+    """ Uma câmara do templo.
+    o explorador usa o método entra para ter acesso aos tesouros
+    """
     def __init__(self):
         self.quantidade = 3
         #self.explorador = explorador
         
     def entra(self, explorador):
         """ entra em uma câmara"""
-        
-        
-        input("Você entra em uma câmara com tesouros!")
-        if input("Continua?").lower() == "s":
+        #input("Você entra em uma câmara com tesouros!")
+        if input("Você entra em uma câmara com tesouros! Continua?").lower() == "s":
             if self.quantidade:
                 self.quantidade -= 1        
                 explorador.pega(randint(1, 4), self)
@@ -67,20 +62,26 @@ class Camara:
                 explorador.sai()
         else:
             explorador.sai()
-        
-
-
+#===========================================================================
 class TemploInca:
+    """ O jogo do Tesouro Inca
+    
+    o jogo inicia quando se chama o método inicia
+    """
     def __init__(self):
         self.explorador = Explorador()
         self.camara = Camara()
         
     def inicia(self):
         """ inicia a exploração """
-        input("Uma expedição para coletar os tesouros do Templo Inca")
-        self.camara.entra(self.explorador)
-        
-
+        encara = input("Uma expedição para coletar os tesouros do Templo Inca. Vai encarar (S/N)?")
+        if encara == "S":
+            self.camara.entra(self.explorador)
+        else:
+            input("Sábia Decisao. Vamos evitar este Templo macabro")
+#===========================================================================
+#===========================================================================
+#===========================================================================
 if __name__ == "__main__":
     TemploInca().inicia()
 
