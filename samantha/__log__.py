a8b54fdaf4dd7e026aaba5b4f292a64c0486990f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:46:36 -0300

    Saved samantha/__log__.py @2019-11-06 13:46

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -347,3 +347,25 @@ SyntaxError: invalid syntax
                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 06 2019 10:46:35.48 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 97
+    TemploInca().inicia()
+  module <module> line 80
+    self.decide[o_que_decidiu]()
+  module <module> line 84
+    self.camara.entra(self.explorador)
+  module <module> line 51
+    self.decide[dec]()           
+AttributeError: 'Camara' object has no attribute 'decide'
+'''},
