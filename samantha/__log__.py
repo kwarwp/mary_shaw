dda65dda532f4ae73e2441bc50d4e7e73668c76d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 12:19:49 -0300

    Saved samantha/__log__.py @2019-11-06 15:19

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -413,3 +413,23 @@ AttributeError: 'Camara' object has no attribute 'decide'
     self.decide[o_que_decidiu.lower()](explorador)
 AttributeError: 'CamaraPerigosa' object has no attribute 'decide'
 '''},
+{'date': 'Wed Nov 06 2019 13:19:48.706 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 146
+    TemploInca().inicia()
+  module <module> line 124
+    self.camara = CamaraPerigosa().adentra(Camara())
+  module <module> line 69
+    camera_outra.adentra(self)
+NameError: name 'camera_outra' is not defined
+'''},
