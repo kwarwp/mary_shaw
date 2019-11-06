f862fdd64fb313715caf572b953f7932e5edf918
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:02:52 -0300

    Saved samantha/main.py @2019-11-06 13:02

--- samantha/main.py
+++ samantha/main.py
@@ -1,135 +1,78 @@
-# mary_shaw.roxanne.main.py
-# from adda.main import JogoTesouroInca as JogoMarilia
+# mary_shaw.samantha.main.py
 """
-    Tesouro Inca
+Uma expedição para coletar os tesouros do Templo Inca
+ --Relato:
+ fui e voltei rico
+ 
+ 19.11.06a - usa defaultdict como uma forma de switch
+ 19.11.06 - troca print por input
 """
-from _spy.vitollino.main import Cena, INVENTARIO, Texto
-from elemento.main import Elemento
-from random import choice
 
-__author__ = "Paulo"
-IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
-MUMIA = "https://i.imgur.com/YGV3CPB.jpg"
-COBRA = "https://i.imgur.com/IeC0gBj.jpg"
-TESOURO = "https://i.imgur.com/xkBdHCE.jpg"
-TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
-ACAMPAMENTO = "https://i.imgur.com/dmSDeDF.jpg"
-
-TUMBA = [COBRA, MUMIA] + [TESOURO]*3
+__author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
+__version__ = "19.11.06a"
+from random import randint
 
 
-class Jogador():
-    """ Um explorador em busca de tesouros """
+class Explorador:
+    """ explora o templo inca"""
+    def __init__(self):  # (self, camara)
+        self.mochila = 0
+        self.cabana = 0
+        # self.camara = camara?
+            
+    def pega(self, quantidade, camara):
+        """ coloca um tesouro na mochila """
+        self.mochila += quantidade
+        input(f"Você coloca {quantidade} pedras na mochila e fica com {self.mochila} tesouros")
+        camara.entra(self)
+                    
+    def sai(self):
+        """ sai do templo """
+        self.cabana, self.mochila = self.mochila, 0
+        input(f"Você sai do templo e guarda {self.cabana} tesouros na cabana!")
+
+
+class Camara:
+    """ Uma câmara do templo.
+    o explorador usa o método entra para ter acesso aos tesouros
+    """
     def __init__(self):
-        """ Inicia com tesouro , com acampamento
-            :param acampamento: o acampamento do jogador
-        """
-        self.tesouro = 0
+        self.quantidade = 3
+        #self.explorador = explorador
+        
+    def entra(self, explorador):
+        """ entra em uma câmara"""
+        #input("Você entra em uma câmara com tesouros!")
+        if input("Você entra em uma câmara com tesouros! Continua?").lower() == "s":
+            if self.quantidade:
+                self.quantidade -= 1        
+                explorador.pega(randint(1, 4), self)
+            else:
+                input("Não havia mais tesouros!")
+                explorador.sai()
+        else:
+            explorador.sai()
+        
+
+
+class TemploInca:
+    """ O jogo do Tesouro Inca
     
-    def ganho(self, valor):
-        """ Aumenta o tesouro com valor equivalente de turquesas 
-            :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
-        """   
-        self.tesouro += valor
-        [INVENTARIO.bota("turquesa", TURQUESA) for _ in range(valor)]
-
-    def desiste(self):
-        """ Remove as pedras do inventario e coloca na cabana """
-
-
-class PedrasPreciosas:
-    """ Pedras que integram o tesouro"""
-    def __init__(self, quantas_pedras=4):
-        """ Inicia aa coleção de padras com uma quantidade
-            :param int quantas_pedras: numero de pedras neste tesouro
-        """
-        # = MODELO Conceitual = 
-        self.tesouro_contabil = quantas_pedras
-        # = VISTA = Modelo visual de como apresentar o tesouro
-        self.limbo_onde_as_pedras_desaparecem = Cena()
-        self.pedras_especificas = [Elemento(
-             TURQUESA, x=50+50*pedra, y=250, w=40, h=40) for pedra in
-             range(self.tesouro_contabil)]
-             
-    def representa(self, local):
-        """ Apresenta as pedras organizadas em um local """
-        for pedra in self.pedras_especificas:
-            pedra.entra(local)
-             
-    def some(self):
-        """ Desaparece com as pedras organizadas em um local """
-        for pedra in self.pedras_especificas:
-            pedra.entra(self.limbo_onde_as_pedras_desaparecem)
-
-class Tesouros(Cena):
-    """ Camaras secretas contendo tesouros """
-    def __init__(self, quantas_pedras=4, acampamento=None, eu=None):
-        """ Inicia a camara contendo umas pedras
-            :param int quantas_pedras: numero de pedras nesta camara
-        """
-        class ProximaCamara:
-            def vai(self):
-                self.pedras = pedras_na_camara = choice(range(1,5))
-                self.c = camara = Tesouros(pedras_na_camara, acampamento, eu)
-                camara.vai()
-                Texto(camara, 
-                      "Você encontra um tesouro!",
-                      foi=self.pega_tesouro).vai()
-            def pega_tesouro(self):
-                eu.ganho(self.pedras)
-                self.pedras = 0
-                self.c.esvazia_camara()
-        self.tesouro = quantas_pedras
-        super().__init__(TESOURO, esquerda=acampamento, direita=ProximaCamara())
-        self.pedras = PedrasPreciosas(quantas_pedras=self.tesouro)
-        self.pedras.representa(self)
-    def esvazia_camara(self):
-        self.tesouro = 0
-        self.pedras.some()
-
-
-class Tumba():
-    """ Um complexo de camaras secretas sob o templo """
-    def __init__(self, acampamento, eu):
-        """ Inicia o complexo de camaras """
-        self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
-        self.inicial = choice(self.tumba)
-
-
-class Acampamento(Cena):
-    """ Um lugar seguro e quentinho para admirar seus ganhos """
-    def __init__(self, cena):
-        """ Cria a cena de um acampamento com tesouros """
-        self.tesouro = 4
-        super().__init__(cena)
-        #self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
-        self.pedras = [Elemento(
-             TURQUESA, x=50+50*pedra, y=250, w=40, h=40, cena=self) for pedra in
-             range(self.tesouro)]
-
-    def ganho(self, valor):
-        """ aumenta o tesouro com um valor de pedras """
-        self.tesouro += valor
-
-
-class JogoTesouroInca:
-    """ Representa o Jogo principal """
+    o jogo inicia quando se chama o método inicia
+    """
     def __init__(self):
-        """ Constroi a cena"""
-        INVENTARIO.inicia()
-        self.acampamento = Acampamento(ACAMPAMENTO)
-        self.eu = Jogador()
-        self.tumba = Tumba(self.acampamento, self.eu)
-        self.cena_do_templo = Cena(
-        IMAGEM_DO_TEMPLO, self.acampamento, direita=self.tumba.inicial)
-        self.acampamento.direita = self.cena_do_templo
+        self.explorador = Explorador()
+        self.camara = Camara()
         
     def inicia(self):
-        """ Inicia a construção do Jogo """
-        self.cena_do_templo.vai()
+        """ inicia a exploração """
+        input("Uma expedição para coletar os tesouros do Templo Inca")
+        self.camara.entra(self.explorador)
         
         
+    
+
+
 if __name__ == "__main__":
-    jogo = JogoTesouroInca()
-    jogo.inicia()
-    #print(JogoTesouroInca,JogoTesouroInca())
+    TemploInca().inicia()
+
