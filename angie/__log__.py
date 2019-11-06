8c3409454e679f5b472a662228c6e784d88cb467
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:20:21 -0300

    Saved angie/__log__.py @2019-11-06 13:20

--- angie/__log__.py
+++ angie/__log__.py
@@ -17,3 +17,21 @@
     self.decide['s'] = self.encara
 AttributeError: 'TemploInca' object has no attribute 'decide'
 '''},
+{'date': 'Wed Nov 06 2019 10:20:19.423 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 91
+    TemploInca().inicia()
+  module <module> line 68
+    self.o_que_decidiu['s'] = self.encara
+AttributeError: 'TemploInca' object has no attribute 'o_que_decidiu'
+'''},
