15f83bec10e6ee19df9545d9165b05fb920d4455
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 19:49:14 -0300

    Created amanda/__log__.py @2019-11-26 22:49

---
+++ amanda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 26 2019 19:49:12.507 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy.vitollino.main import Cena, Elemento, INVENTARIO, STYLE
+                                                       ^
+SyntaxError: invalid syntax
+'''},
