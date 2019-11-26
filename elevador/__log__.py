a16834b83418099435ca02260103ecd00c27c58f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 20:08:52 -0300

    Saved elevador/__log__.py @2019-11-26 23:08

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -6,3 +6,10 @@
                                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 26 2019 20:08:52.66 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 29
+  INVENTARIO.score(casa="elevador", carta=self.na_cesta, move="desce", ponto=0, valor=0, _level=1):
+                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
