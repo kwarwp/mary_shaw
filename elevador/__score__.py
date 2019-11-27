36f918eacb8eb38b398886c19b41018fb8824be5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:28:22 -0300

    Saved elevador/__score__.py @2019-11-27 21:28

--- elevador/__score__.py
+++ elevador/__score__.py
@@ -30,3 +30,4 @@
 {'doc_id': '87dc65d3', 'carta': 'nada', 'casa': 'elevador', 'move': 'desce', 'ponto': 0, 'valor': 0, '_level': 0, 'tempo': 1574889831346},
 {'doc_id': 'dff0349e', 'carta': 'nada', 'casa': 'elevador', 'move': 'desce', 'ponto': 0, 'valor': 0, '_level': 0, 'tempo': 1574889927952},
 {'doc_id': '21384848', 'carta': 'nada', 'casa': 'elevador', 'move': 'desce', 'ponto': 0, 'valor': 0, '_level': 0, 'tempo': 1574889983287},
+{'doc_id': '21384848', 'carta': 'nada', 'casa': 'elevador', 'move': 'desce', 'ponto': 0, 'valor': 0, '_level': 0, 'tempo': 1574890102018},
