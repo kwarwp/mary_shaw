c1006c2fb76aee5c22abe42e65f85a2dc6526ef6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:19:01 -0300

    Saved elevador/__log__.py @2019-11-27 21:19

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -49,3 +49,21 @@ NameError: name 'Texto' is not defined
     b = Texto(predio, Popup.POP.optou)
 NameError: name 'Popup' is not defined
 '''},
+{'date': 'Wed Nov 27 2019 18:19:00.62 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 65
+    Elevador()
+  module <module> line 23
+    a = Texto(predio, "oi", A="ee", B="uu")
+TypeError: 'module' object is not callable
+'''},
