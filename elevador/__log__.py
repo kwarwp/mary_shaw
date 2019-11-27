74262c717272b5195fe42b4579528e60220f23ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:20:54 -0300

    Saved elevador/__log__.py @2019-11-27 21:20

--- elevador/__log__.py
+++ elevador/__log__.py
@@ -67,3 +67,23 @@ NameError: name 'Popup' is not defined
     a = Texto(predio, "oi", A="ee", B="uu")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Nov 27 2019 18:20:53.701 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from texto.main import Texto
+  module texto.main line 105
+    class Texto(Popup):
+  module texto.main line 106
+    def __init__(self, cena=NADA, tit="", txt="", foi=None, **kwargs):
+NameError: name 'NADA' is not defined
+'''},
