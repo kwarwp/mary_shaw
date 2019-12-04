2ce1790b31d1f053237124e5ffa8bfac8a3e74ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:10:41 -0300

    Created alexa/__log__.py @2019-12-04 15:10

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Dec 04 2019 12:10:38.534 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  pass
+  ^
+IndentationError: expected an indented block
+'''},
