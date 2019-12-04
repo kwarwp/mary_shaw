b35cb54940662b1515b03c6165f24a69a8fb5e65
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:45:11 -0300

    Saved alexa/__log__.py @2019-12-04 15:45

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Dec 04 2019 12:45:10.737 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  def move(self, evento = None)
+                                ^
+SyntaxError: invalid syntax
+'''},
