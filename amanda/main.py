1906192ecf0d05b110a78d73d4231c4248bb74d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 11:59:10 -0300

    Created amanda/main.py @2019-12-04 14:59

---
+++ amanda/main.py
@@ -0,0 +1,24 @@
+# mary_shaw.amanda.main.py
+# mary_shaw.basico.main.py
+# Este jogo é um software livre com licensa GPL3 `GPL <http://is.gd/3Udt>`__.
+"""
+Demonstração de elementos para jogo de transporte
+"""
+__author__ = "Carlo Oliveira"
+__version__ = "19.12.03"
+from _spy.vitollino.main import Cena,Elemento,Texto, STYLE, INVENTARIO
+
+STYLE["width"]=900
+STYLE["height"]= "600px"
+IGR = "https://i.imgur.com/"
+CART, CAT, BASE, CENA = f"{IGR}m2k5sv6.png", f"{IGR}ek8oINR.png", f"{IGR}DAUyvBP.jpg", f"{IGR}nkwZCrR.jpg"
+
+
+class Basico:
+    def __init__(self):
+        self.cena = cena = Cena(CENA)
+        cena.vai()
+        
+        
+if __name__ == "__main__":
+    Basico()
