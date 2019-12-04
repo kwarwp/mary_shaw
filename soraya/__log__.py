e6c91c6c63140a6a729d90e5afafe105a60c018e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:50:08 -0300

    Saved soraya/__log__.py @2019-12-04 15:50

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -294,3 +294,21 @@ SyntaxError: invalid syntax
                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Dec 04 2019 12:50:07.28 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 49
+    Basico()
+  module <module> line 41
+    self.base0 = plataforna(BASE, x=100,cena=cena)
+NameError: name 'plataforna' is not defined
+'''},
