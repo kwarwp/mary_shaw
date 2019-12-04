6a9f3be5d9d859b76fe1ac458a357ca589a59b3a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:55:36 -0300

    Saved rachel/__log__.py @2019-12-04 15:55

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1293,3 +1293,23 @@ Traceback (most recent call last):
     print(place)
 NameError: name 'place' is not defined
 '''},
+{'date': 'Wed Dec 04 2019 12:55:35.851 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 106
+    Basico()
+  module <module> line 93
+    self.m0.put(self.monster)
+  module <module> line 39
+    self.place[person.getId()] = true
+NameError: name 'true' is not defined
+'''},
