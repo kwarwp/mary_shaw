b0fa5908c1d6448ec16f1698e36cfe0d4d5f39b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:12:24 -0300

    Saved adda/main.py @2019-12-04 15:12

--- adda/main.py
+++ adda/main.py
@@ -7,61 +7,36 @@ e podiamos escolher entre ficar ou sair.
 """
 __autor__ = "Marília Campos Galvão"
 
-from random import randint
+__version__ = "19.12.03"
+from _spy.vitollino.main import Cena,Elemento,Texto, STYLE, INVENTARIO
 
-class Explorador:
-    def __init__(self, templo_inca):
-        self.mochila = 0
-        self.cabana = 0
-        self.templo = templo_inca
-        
-    def pega(self, quantidade, camara):
-        """ guarda um tesouro na machila """
-        print(f"Você coloca {quantidade} tesouro na mochila" )
-        self.mochila += quantidade
-        print(f"Você fica com {self.mochila} tesouros na mochila")
-        if input("Continua? (s/N)").lower() == "s":
-            camara.entra(self)
-        else:
-            self.sai()
-        
-    def sai(self):
-        """ sai do templo """
-        print("Você sai do templo e guarda os tesouros!")
-        self.cabana += self.mochila
-        self.mochila = 0
-        print(f"Você ficou com {camara.pedras} tesouros na mochila")
-        if input("Nova Incursão? (s/N)").lower() == "s":
-            self.templo.inicia()
-class Camara:
+STYLE["width"]=900
+STYLE["height"]= "600px"
+IGR = "https://i.imgur.com/"
+CART, CAT, BASE, CENA = f"{IGR}m2k5sv6.png", f"{IGR}ek8oINR.png", f"{IGR}DAUyvBP.jpg", f"{IGR}nkwZCrR.jpg"
+
+class Personagen(Elemento):
+    def __init__(self,imagem, cena):
+        pass
+
+class Veiculo(Elemento):
+    def __init__(self,imagem, cena):
+        pass
+
+class Plataforma(Elemento):
+    def __init__(self,imagem, cena):
+    #super().__init__(???/)
+        pass
+
+
+class Basico:
     def __init__(self):
-        self.pedras = 3
-        
-    def entra(self, explorador):
-        """ entra em uma câmera"""
-        print("O que existe nela")
-        if self.pedras:
-            self.pedras -= 1
-            explorador.pega(randint(1, 4), self)
-        else:
-            print(f"A rodada acabou para você, leve suas {explorador.cabana} para cabana")
-            explorador.sai()
-
-class TemploInca:
-    def __init__(self):
-        self.explorador = Explorador(templo_inca=self)
-        self.camara = Camara(self.explorador)
-
-        
-    def inicia(self):
-        """ inicia a exploração """
-        print("Uma expedição para coletar os tesouros de um templo Inca")
-        self.camara.entra(self.explorador)
-        
-    
-            
-    
+        self.cena = cena = Cena(CENA)
+        self.base0 = Elemento(BASE, cena=cena)
+        self.gato = Elemento(CAT, cena=cena)
+        self.cart = Elemento(CART, cena=cena)
+        cena.vai()
         
         
 if __name__ == "__main__":
-    TemploInca().inicia()
+    Basico()
