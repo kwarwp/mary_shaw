17f9e497c0561469b31703495718fe7249d16d27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:47:14 -0300

    Saved alexa/__log__.py @2019-12-04 15:47

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -38,3 +38,21 @@ NameError: name 'cart' is not defined
                                                                         ^
 SyntaxError: keyword can't be an expression
 '''},
+{'date': 'Wed Dec 04 2019 12:47:12.883 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    Basico()
+  module <module> line 41
+    self.gato = Elemento(CAT, cena=cena, x=700, y=200,w=60,h=60, vai = self.cart)
+AttributeError: 'Basico' object has no attribute 'cart'
+'''},
