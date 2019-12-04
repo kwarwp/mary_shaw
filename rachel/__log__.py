a43df8b20f6e80937cf6c9f34834004b07b49f30
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:22:44 -0300

    Saved rachel/__log__.py @2019-12-04 15:22

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1136,3 +1136,21 @@ Exception: <RangeError: Maximum call stack size exceeded>
     place[MONSTER] = ""
 NameError: name 'place' is not defined
 '''},
+{'date': 'Wed Dec 04 2019 12:22:43.172 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    class Plataforma(Elemento):
+  module <module> line 24
+    self.place[MONSTER] = ""
+AttributeError: 'GUI' object has no attribute 'place'
+'''},
