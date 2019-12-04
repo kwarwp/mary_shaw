161825ffb3119b6a09ed1a212912c7a772e438a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:10:38 -0300

    Saved soraya/main.py @2019-12-04 15:10

--- soraya/main.py
+++ soraya/main.py
@@ -1,94 +1,40 @@
-# mary_shaw.samantha.main.py
+# mary_shaw.amanda.main.py
+# mary_shaw.basico.main.py
+# Este jogo é um software livre com licensa GPL3 `GPL <http://is.gd/3Udt>`__.
 """
-Uma expedição para coletar os tesouros do Templo Inca
- --Relato:
- fui e voltei rico
- 
- 19.11.06b - usa defaultdict na Camara também
- 19.11.06a - usa defaultdict como uma forma de switch
- 19.11.06 - troca print por input
+Demonstração de elementos para jogo de transporte
 """
+__author__ = "Carlo Oliveira"
+__version__ = "19.12.03"
+from _spy.vitollino.main import Cena,Elemento,Texto, STYLE, INVENTARIO
 
-__author__ = "Savyo V Morais <savyo dot morais at labnet at nce at ufrj at br>"
-__version__ = "19.11.06b"
-from random import randint
-from collections import defaultdict
+STYLE["width"]=900
+STYLE["height"]= "600px"
+IGR = "https://i.imgur.com/"
+CART, CAT, BASE, CENA = f"{IGR}m2k5sv6.png", f"{IGR}ek8oINR.png", f"{IGR}DAUyvBP.jpg", f"{IGR}nkwZCrR.jpg"
 
+class Personagen(Elemento):
+    def __init__(self,imagem, cena):
+    pass
 
-class Explorador:
-    """ explora o templo inca"""
-    def __init__(self):  # (self, camara)
-        self.mochila = 0
-        self.cabana = 0
-        # self.camara = camara?
-            
-    def pega(self, quantidade, camara):
-        """ coloca um tesouro na mochila """
-        self.mochila += quantidade
-        input(f"Você coloca {quantidade} pedras na mochila e fica com {self.mochila} tesouros")
-        camara.entra(self)
-                    
-    def sai(self):
-        """ sai do templo """
-        self.cabana, self.mochila = self.mochila, 0
-        input(f"Você sai do templo e guarda {self.cabana} tesouros na cabana!")
+class Veiculo(Elemento):
+    def __init__(self,imagem, cena):
+    pass
+
+class Plataforma(Elemento):
+    def __init__(self,imagem, cena):
+    #super().__init__(???/)
+    pass
 
 
-class Camara:
-    """ Uma câmara do templo.
-    o explorador usa o método entra para ter acesso aos tesouros
-    """
+class Basico:
     def __init__(self):
-        self.quantidade = 3
-        #self.explorador = explorador
-        
-    def entra(self, explorador):
-        """ entra em uma câmara"""
-        #input("Você entra em uma câmara com tesouros!")
-        if input("Você entra em uma câmara com tesouros! Continua?").lower() == "s":
-            if self.quantidade:
-                self.quantidade -= 1        
-                explorador.pega(randint(1, 4), self)
-            else:
-                input("Não havia mais tesouros!")
-                explorador.sai()
-        else:
-            explorador.sai()
-        
-
-
-class TemploInca:
-    """ O jogo do Tesouro Inca
-    
-    o jogo inicia quando se chama o método inicia
-    """
-    def __init__(self):
-        self.explorador = Explorador()
-        self.camara = Camara()
-        self.decide = defaultdict(lambda: self.desiste)
-        self.decide["s"] = self.encara
-        '''
-        self.decide = defaultdict(lambda: input("Sábia mimimi.. macabro!"))
-        self.decide["s"] = lambda: self.camara.entra(self.explorador)
-        '''
-        
-    def inicia(self):
-        """ inicia a exploração """
-        o_que_decidiu = input("Uma expedição para saquear o Templo Inca. Vai encarar (s/N)?")
-        self.decide[o_que_decidiu]()
-        
-    def encara(self):
-        """ decide iniciar a exploração """
-        self.camara.entra(self.explorador)
-        
-    def desiste(self):
-        """ desiste da exploração """
-        input("Sábia decisão, vamos evitar este templo macabro!")
+        self.cena = cena = Cena(CENA)
+        self.base0 = Elemento(BASE, cena=cena)
+        self.gato = Elemento(CAT, cena=cena)
+        self.cart = Elemento(CART, cena=cena)
+        cena.vai()
         
         
-    
-
-
 if __name__ == "__main__":
-    TemploInca().inicia()
-
+    Basico()
