8b22054f9031522dd235e4ba5b5ed2c91e43ac94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 12:52:46 -0300

    Saved alexa/main.py @2019-12-04 15:52

--- alexa/main.py
+++ alexa/main.py
@@ -1,7 +1,4 @@
-# mary_shaw.alexa.main.py
-# mary_shaw.amanda.main.py
 # mary_shaw.basico.main.py
-# Autor: Lorena, Horty e Marilia
 # Este jogo é um software livre com licensa GPL3 `GPL <http://is.gd/3Udt>`__.
 """
 Demonstração de elementos para jogo de transporte
@@ -15,31 +12,36 @@ STYLE["height"]= "600px"
 IGR = "https://i.imgur.com/"
 CART, CAT, BASE, CENA = f"{IGR}m2k5sv6.png", f"{IGR}ek8oINR.png", f"{IGR}DAUyvBP.jpg", f"{IGR}nkwZCrR.jpg"
 
+
 class Plataforma(Elemento):
-    def __init__(self,imagem, cena, x=0, y=200):
+    def __init__(self, imagem, cena, x=0, y=400):
         super().__init__(imagem, cena=cena, x=x, y=y)
-        
-    def move(self, evento = None)
-        #self.elt.style.transition = "all"
-        
-class Personagen(Elemento):
-    def __init__(self,imagem, cena):
+        #self.elt.style.transition = "all 1s"
         pass
+
+
+class Personagem(Elemento):
+    def __init__(self, imagem, veiculo, cena, x=0, y=400):
+        super().__init__(imagem, cena=cena, x=x, y=y)
+        self.veiculo = veiculo
+        self.vai = self.move
+        
+    def move(self, evento=None):
+        self.entra(self.veiculo)
+
 
 class Veiculo(Elemento):
-    def __init__(self,imagem, cena):
-        pass
-
+    def __init__(self, imagem, cena, x=0, y=400):
+        super().__init__(imagem, cena=cena, x=x, y=y)
 
 
 class Basico:
     def __init__(self):
         self.cena = cena = Cena(CENA)
-        self.base0 = Elemento(BASE, cena=cena, x=200, y=200, w=100,h=100)
-        self.base1 = Elemento(BASE, cena=cena, x=700, y=200, w=100,h=100)
-        
-        self.gato = Elemento(CAT, cena=cena, x=700, y=200,w=60,h=60)
-        self.cart = Elemento(CART, cena=cena, x=600, y=200,w=90,h=90)
+        self.base0 = Plataforma(BASE, x=100, cena=cena)
+        self.base1 = Plataforma(BASE, x=500, cena=cena)
+        self.cart = Veiculo(CART, x=100, cena=cena)
+        self.gato = Personagem(CAT, veiculo=self.cart, cena=cena)
         cena.vai()
         
         
