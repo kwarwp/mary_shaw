8097bfcf05a03aeb6008a930449e3bf1062588f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 21:20:13 -0300

    Saved rachel/__log__.py @2019-12-05 00:20

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1356,3 +1356,21 @@ SyntaxError: non-keyword arg after keyword arg
     self.boat = Boat(IMG_BOAT_TO_RIGHT, 100, 0, self.left_margin, self.right_margin)
 TypeError: __init__ missing 1 positional argument: 'right_margin'
 '''},
+{'date': 'Wed Dec 04 2019 21:20:12.919 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Game()
+  module <module> line 125
+    self.monster = Character(IMG_MONSTER, cena, MONSTER, self.left_margin)
+NameError: name 'cena' is not defined
+'''},
