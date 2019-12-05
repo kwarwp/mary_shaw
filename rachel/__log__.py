a29f7a0425c57cdaa5534ee61eab9660317b4f00
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 21:25:02 -0300

    Saved rachel/__log__.py @2019-12-05 00:25

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1392,3 +1392,21 @@ NameError: name 'cena' is not defined
     self.monster = Character(IMG_MONSTER, self.cena, MONSTER, self.left_margin)
 TypeError: __init__ missing 2 positional arguments: 'name' and 'margin'
 '''},
+{'date': 'Wed Dec 04 2019 21:25:01.888 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Game()
+  module <module> line 126
+    self.dwarf = Character(IMG_DWARF, self.cena, 0, 0, DWARF, self.left_margin)
+TypeError: __init__ missing 2 positional arguments: 'name' and 'margin'
+'''},
