c9563801205459da8583900e318fe5ddfb463ab8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 11:07:18 -0300

    Saved rachel/__log__.py @2019-12-05 14:07

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1672,3 +1672,23 @@ NameError: name 'LEFT_MARGIN_y' is not defined
     printStatus()
 NameError: name 'printStatus' is not defined
 '''},
+{'date': 'Thu Dec 05 2019 11:07:17.576 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 261
+    Game()
+  module <module> line 249
+    self.boat.click()
+  module <module> line 126
+    printStatus()
+NameError: name 'printStatus' is not defined
+'''},
