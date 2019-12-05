2b136a21a788c04403d26a86d9941027bcbf5f57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 09:26:29 -0300

    Saved rachel/__log__.py @2019-12-05 12:26

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1468,3 +1468,23 @@ AttributeError: 'Character' object has no attribute 'boat'
     self.style.update(**style)
 TypeError: 'int' object is not iterable
 '''},
+{'date': 'Thu Dec 05 2019 09:26:28.750 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Game()
+  module <module> line 156
+    self.monster_left_slot = Elemento(None, self.cena, MONSTER_LEFT_SLOT_X, MONSTER_LEFT_SLOT_Y, MONSTER_W, MONSTER_H)
+  module _spy.vitollino.main line 538
+    self.style.update(**style)
+TypeError: 'int' object is not iterable
+'''},
