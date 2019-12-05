c1595ff81cea8268b5455c209eee64a5552e0a67
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 21:18:25 -0300

    Saved rachel/__log__.py @2019-12-05 00:18

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1313,3 +1313,21 @@ NameError: name 'place' is not defined
     self.place[person.getId()] = true
 NameError: name 'true' is not defined
 '''},
+{'date': 'Wed Dec 04 2019 21:18:24.335 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Game()
+  module <module> line 122
+    self.boat = Boat(IMG_BOAT_TO_RIGHT, self.left_margin, self.right_margin)
+TypeError: __init__ missing 2 positional arguments: 'left_margin' and 'right_margin'
+'''},
