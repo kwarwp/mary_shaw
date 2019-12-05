81908f3396ed2d55b3266eeb798eea8defe568d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 09:42:15 -0300

    Saved rachel/__log__.py @2019-12-05 12:42

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1540,3 +1540,23 @@ SyntaxError: invalid syntax
                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Dec 05 2019 09:42:14.563 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 221
+    Game()
+  module <module> line 161
+    self.monster_left_slot = Elemento(None, self.cena, MONSTER_LEFT_SLOT_X, MONSTER_LEFT_SLOT_Y, MONSTER_W, MONSTER_H)
+  module _spy.vitollino.main line 538
+    self.style.update(**style)
+TypeError: 'int' object is not iterable
+'''},
