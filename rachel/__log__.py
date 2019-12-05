51a371cbe86244cf528be35456a79eb5e558a59e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 21:19:45 -0300

    Saved rachel/__log__.py @2019-12-05 00:19

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1338,3 +1338,21 @@ TypeError: __init__ missing 2 positional arguments: 'left_margin' and 'right_mar
                                                                   ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Wed Dec 04 2019 21:19:44.219 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Game()
+  module <module> line 122
+    self.boat = Boat(IMG_BOAT_TO_RIGHT, 100, 0, self.left_margin, self.right_margin)
+TypeError: __init__ missing 1 positional argument: 'right_margin'
+'''},
