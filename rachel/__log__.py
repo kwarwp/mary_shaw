2dc69d44a80225fca5e6ee2a068806f546ec7c89
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 09:27:24 -0300

    Saved rachel/__log__.py @2019-12-05 12:27

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1508,3 +1508,21 @@ TypeError: 'int' object is not iterable
     self.left_slot = slot
 NameError: name 'slot' is not defined
 '''},
+{'date': 'Thu Dec 05 2019 09:27:23.188 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Game()
+  module <module> line 166
+    self.dwarf = Character(IMG_DWARF, self.cena, DWARF_LEFT_SLOT_X, DWARF_LEFT_SLOT_Y, DWARF_W, DWARF_H, DWARF, self.left_margin, self.boat)
+TypeError: __init__ missing 1 positional argument: 'boat'
+'''},
