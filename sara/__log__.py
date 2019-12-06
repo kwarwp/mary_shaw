626060fd8322a345be8e20610d13e8608379195b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 22:30:10 -0300

    Saved sara/__log__.py @2019-12-06 01:30

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,10 @@
       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Dec 05 2019 22:30:09.294 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 173
+  "
+   ^
+SyntaxError: EOL while scanning string literal
+'''},
