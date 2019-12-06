55de04353e9e7ec315aeb500da06b6665a10376f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 22:29:42 -0300

    Created sara/__log__.py @2019-12-06 01:29

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Dec 05 2019 22:29:40.347 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 173
+  "" antes
+      ^
+SyntaxError: invalid syntax
+'''},
