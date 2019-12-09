f162045984c0454c767fd63effa4ca5a3307a871
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-09 11:09:14 -0300

    Saved rachel/__log__.py @2019-12-09 14:09

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -1692,3 +1692,23 @@ NameError: name 'printStatus' is not defined
     printStatus()
 NameError: name 'printStatus' is not defined
 '''},
+{'date': 'Mon Dec 09 2019 11:09:13.793 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 309
+    Game()
+  module <module> line 226
+    self.showStartScreen()
+  module <module> line 292
+    self.cena = Cena(CENA_START)
+NameError: name 'CENA_START' is not defined
+'''},
