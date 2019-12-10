183ead0898e5b759d54f3cec51a66f37610f10e2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-10 15:33:10 -0300

    Saved basico/__log__.py @2019-12-10 18:33

--- basico/__log__.py
+++ basico/__log__.py
@@ -41,3 +41,25 @@ AttributeError: 'Basico' object has no attribute 'cart'
     self.fundo.entra(passageiro)
 AttributeError: 'Veiculo' object has no attribute 'fundo'
 '''},
+{'date': 'Tue Dec 10 2019 15:33:09.217 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    Basico()
+  module <module> line 72
+    self.cart0 = Veiculo(CART, destino=self.base1, cena=self.base0)
+  module <module> line 40
+    self.fundo = Elemento(imagem, cena=self, x=x, y=y)
+  module _spy.vitollino.main line 541
+    self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
+AttributeError: 'Veiculo' object has no attribute 'nome'
+'''},
