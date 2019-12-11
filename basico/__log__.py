618b9bd63f21af8cdd822b63b0121908dbd03faf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-11 19:32:42 -0300

    Saved basico/__log__.py @2019-12-11 22:32

--- basico/__log__.py
+++ basico/__log__.py
@@ -107,3 +107,21 @@ AttributeError: 'Veiculo' object has no attribute 'elt'
     self.cart0.aporta(self.base0)
 AttributeError: 'Veiculo' object has no attribute 'aporta'
 '''},
+{'date': 'Wed Dec 11 2019 19:32:41.421 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 87
+    Basico()
+  module <module> line 78
+    self.base1.aporta(self.cart1)
+AttributeError: 'Basico' object has no attribute 'cart1'
+'''},
