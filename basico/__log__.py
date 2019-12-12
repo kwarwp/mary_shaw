8aaf9e93eacc02487965e0527e81c5f729029f36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 14:36:22 -0300

    Saved basico/__log__.py @2019-12-12 17:36

--- basico/__log__.py
+++ basico/__log__.py
@@ -168,3 +168,21 @@ AttributeError: 'Basico' object has no attribute 'base1'
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Dec 12 2019 14:36:21.686 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 90
+    Basico()
+  module <module> line 77
+    self.base0 = Plataforma(BASE, x=100, cena=cena)
+UnboundLocalError: local variable 'cena' referenced before assignment
+'''},
