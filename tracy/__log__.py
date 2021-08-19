77ea1c021e71ddebc4fe3077d8e5fb201b46c63f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-19 13:10:56 -0300

    Saved tracy/__log__.py @2021-08-19 16:10

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -1016,3 +1016,31 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Aug 19 2021 13:10:55.826 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 156
+    jogo = JogoTesouroInca()
+  module <module> line 145
+    self.tumba = Tumba(self.acampamento, self.eu)
+  module <module> line 118
+    self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+  module <module> line 107
+    self.pedras = PedrasPreciosas(quantas_pedras=self.tesouro)
+  module <module> line 73
+    self.pedras_especificas = [Elemento(
+  module elemento.main line 19
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
