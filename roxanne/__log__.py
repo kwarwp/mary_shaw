3a0a582aeb6017011162a9ed7d6b704584d06a89
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-21 15:44:36 -0300

    Saved roxanne/__log__.py @2022-03-21 18:44

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -184,3 +184,27 @@ NameError: name 'TesouroS' is not defined
     super().__init__(cena)
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Mon Mar 21 2022 15:44:40.937 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    jogo = JogoTesouroInca()
+  module <module> line 120
+    self.acampamento = Acampamento(ACAMPAMENTO)
+  module <module> line 106
+    self.pedras = [Elemento(
+  module elemento.main line 19
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
